8de8736f822dc6c08109454d207b0cd6c62e9c0b
0gene68 <yeonghun8309@gmail.com>
2024-02-01 20:56:10 +0900

    feat: 네비게이션 바 각 탭 페이지 생성

---
+++ screens/Chat.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Chat() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Chat Page</Text>
+    </View>
+  );
+}
+
+export default Chat;
---
+++ screens/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import NavBar from "../NavBar";
+
+function Main() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Main Page</Text>
+    </View>
+  );
+}
+
+export default Main;
