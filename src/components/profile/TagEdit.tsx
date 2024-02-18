00d21f9349375101f9d1c9e0fb760769122e0ac1
0gene68 <yeonghun8309@gmail.com>
2024-02-18 23:21:27 +0900

    feat: 태그 편집 임시 페이지 생성

---
+++ src/components/profile/TagEdit.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function Test() {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center" }}>
+      <Text>Tag Edit Page</Text>
+    </View>
+  );
+}
+
+export default Test;
