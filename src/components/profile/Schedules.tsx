49ff25bd0f955bb891647dbcf444bff8802aebb8
0gene68 <yeonghun8309@gmail.com>
2024-02-19 20:13:33 +0900

    feat: 여행 일정 임시 페이지

---
+++ src/components/profile/Schedules.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function Schedules() {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center" }}>
+      <Text>All Schedules Page</Text>
+    </View>
+  );
+}
+
+export default Schedules;
