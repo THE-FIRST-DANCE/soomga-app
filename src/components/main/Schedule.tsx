0a8a2e71e5025fe74417a04a6a09a37a74aab1b8
0gene68 <yeonghun8309@gmail.com>
2024-02-19 20:01:15 +0900

    chore: 주석 추가

--- src/components/main/Schedule.tsx
+++ src/components/main/Schedule.tsx
@@ -9,30 +9,20 @@ type ScheduleType = {
   date_end: string;
 };
 
-function Schedule({
-  id,
-  name,
-  description,
-  date_start,
-  date_end,
-}: ScheduleType) {
+function Schedule({ name, description, date_start, date_end }: ScheduleType) {
   return (
-    <TouchableOpacity
-      style={styles.scheduleContainer}
-      onPress={() => console.log(`Schedule ${id} button Pressed!`)}
-    >
+    <TouchableOpacity style={styles.container}>
+      {/* 좌측 바 */}
       <View style={styles.leftBar} />
       <View
         style={{
           flexDirection: "row",
         }}
       >
-        <Text
-          // style={{ ...styles.scheduleText, fontSize: 20, fontWeight: "700" }}
-          style={[styles.scheduleText, styles.name]}
-        >
-          {name}
-        </Text>
+        {/* 일정 이름 */}
+        <Text style={[styles.scheduleText, styles.name]}>{name}</Text>
+
+        {/* 일정 설명 */}
         <Text
           style={[styles.scheduleText, styles.description]}
           numberOfLines={1}
@@ -42,7 +32,9 @@ function Schedule({
         </Text>
       </View>
       <View style={{ marginVertical: 10, position: "absolute", right: 10 }}>
+        {/* 일정 시작 시각 */}
         <Text style={styles.date}>{date_start}</Text>
+        {/* 일정 종료 시각 */}
         <Text style={styles.date}>{date_end}</Text>
       </View>
     </TouchableOpacity>
@@ -52,33 +44,39 @@ function Schedule({
 export default Schedule;
 
 const styles = StyleSheet.create({
-  scheduleContainer: {
+  /* 컨테이너 스타일 */
+  container: {
     flexDirection: "row",
     marginTop: 5,
     marginHorizontal: 3,
     borderRadius: 10,
   },
+  /* 좌측 바 스타일 */
   leftBar: {
     backgroundColor: "yellowgreen",
     width: 5,
     height: 50,
     borderRadius: 10,
   },
+  /* 이름, 설명, 시각 공통 스타일 */
   scheduleText: {
     height: 50,
     marginHorizontal: 10,
     alignItems: "center",
     lineHeight: 50,
   },
+  /* 일정 이름 스타일 */
   name: {
     width: 90,
     fontSize: 20,
     fontWeight: "700",
   },
+  /* 일정 설명 스타일 */
   description: {
     fontSize: 10,
     width: 115,
   },
+  /* 일정 시각 스타일 */
   date: {
     fontSize: 10,
     lineHeight: 15,
