0c1be27f4a91660528d52c39ce1fa43d7a74ca31
0gene68 <yeonghun8309@gmail.com>
2024-02-20 17:23:06 +0900

    feat: 현재 위치 정보 렌더링

--- src/components/main/recommend/Recommend.tsx
+++ src/components/main/recommend/Recommend.tsx
@@ -1,18 +1,79 @@
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { View, Text, StyleSheet, Dimensions, Alert } from "react-native";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 /* Pages */
 import Sights from "./Sights";
 
 /* vector-icons */
-import { Ionicons } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
+
+/* 위치 정보 객체 타입 */
+type LocationType = {
+  city: string | null;
+  region: string | null;
+  street: string | null;
+};
 
 function Recommend() {
+  /* 비동기 작업 진행 여부 변수 */
+  const [isLoading, setIsLoading] = useState(true);
+
+  /* 위치 정보 저장 변수 */
+  const [location, setLocation] = useState<LocationType>({
+    city: "",
+    region: "",
+    street: "",
+  });
+
+  /* 위치 정보 가져오는 함수 */
+  const getLocation = async () => {
+    try {
+      /* 전경 위치 권한 요청 */
+      await Location.requestForegroundPermissionsAsync();
+
+      /* 현재 위도, 경도 저장 */
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({
+        accuracy: 5,
+      });
+
+      /* 위도, 경도를 이용해 주소 저장 */
+      const location = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+
+      /* 비동기 작업 종료 */
+      setIsLoading(false);
+
+      /* 얻은 주소 정보 저장 */
+      setLocation({
+        city: location[0].city,
+        region: location[0].region,
+        street: location[0].street,
+      });
+    } catch (e) {
+      Alert.alert("위치 정보를 가져올 수 없습니다.");
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>여행지 추천</Text>
       <View style={styles.locationContainer}>
-        <Ionicons name="location-sharp" size={30} color="black" />
-        <Text style={styles.location}>대구광역시 북구 복현동</Text>
+        <SimpleLineIcons name="location-pin" size={24} color="gray" />
+        <Text style={styles.location}>
+          {/* 비동기 작업 진행중일 경우 메시지를 먼저 띄움 */}
+          {isLoading
+            ? "위치 정보 불러오는 중..."
+            : `${location.region} ${location.city} ${location.street}`}
+        </Text>
       </View>
       <View>
         <Sights />
@@ -23,14 +84,20 @@ function Recommend() {
 
 const screenWidth = Dimensions.get("window").width;
 const styles = StyleSheet.create({
+  /* 추천 전체 스타일 */
   container: {
     width: screenWidth - 30,
-    marginTop: 10,
+    marginTop: 20,
     marginHorizontal: 15,
   },
+  /* 제목 스타일 */
   title: { fontWeight: "700", fontSize: 25, marginLeft: 5 },
+
+  /* 위치 정보 컨테이너 스타일 */
   locationContainer: { flexDirection: "row", height: 30, marginTop: 10 },
-  location: { fontSize: 15, lineHeight: 26, marginLeft: 5 },
+
+  /* 위치 정보 텍스트 스타일 */
+  location: { fontSize: 14, lineHeight: 25, marginLeft: 5 },
 });
 
 export default Recommend;
