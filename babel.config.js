4f0f3cf84f50a70d540fc0adf42263362d9975dc
0gene68 <yeonghun8309@gmail.com>
2024-02-21 19:18:00 +0900

    style: path alias 지정

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@/*": "./src/*",
+            "@components/*": "./src/components/*",
+            "@main/*": "./src/components/main/*",
+            "@recommend/*": "./src/components/main/recommend/*",
+            "@schedule/*": "./src/components/main/Schedule/*",
+            "@profile/*": "./src/components/profile/*",
+            "@navigation/*": "./src/navigation/*",
+          },
+        },
+      ],
+    ],
   };
 };
