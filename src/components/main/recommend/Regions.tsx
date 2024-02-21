b97a60dadfc3eea4447017379d3f9575c548252a
0gene68 <yeonghun8309@gmail.com>
2024-02-21 20:33:55 +0900

    feat: 지역 추천 컨테이너 추가

---
+++ src/components/main/recommend/Regions.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+function Regions() {
+  return (
+    <View>
+      <Text style={{ borderWidth: 1, width: 350, height: 50 }}>
+        Regions Recommend Container
+      </Text>
+    </View>
+  );
+}
+
+export default Regions;
