d408cb679790c9ef6ff91603cfbb8d3dd8d20a9c
0gene68 <yeonghun8309@gmail.com>
2024-02-22 15:40:04 +0900

    chore: 여행 일정 컴포넌트 UI 조정

--- src/components/main/Schedule/Schedule.tsx
+++ src/components/main/Schedule/Schedule.tsx
@@ -11,14 +11,10 @@ type ScheduleType = {
 
 function Schedule({ name, description, start_time, end_time }: ScheduleType) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <View style={styles.container}>
       {/* 좌측 바 */}
       <View style={styles.leftBar} />
-      <View
-        style={{
-          flexDirection: "row",
-        }}
-      >
+      <TouchableOpacity style={styles.scheduleInformation}>
         {/* 일정 이름 */}
         <Text style={[styles.scheduleText, styles.name]}>{name}</Text>
 
@@ -30,14 +26,14 @@ function Schedule({ name, description, start_time, end_time }: ScheduleType) {
         >
           {description}
         </Text>
-      </View>
-      <View style={{ marginVertical: 10, position: "absolute", right: 10 }}>
-        {/* 일정 시작 시각 */}
-        <Text style={styles.date}>{start_time}</Text>
-        {/* 일정 종료 시각 */}
-        <Text style={styles.date}>{end_time}</Text>
-      </View>
-    </TouchableOpacity>
+        <View style={{ marginVertical: 10, position: "absolute", right: 10 }}>
+          {/* 일정 시작 시각 */}
+          <Text style={styles.date}>{start_time}</Text>
+          {/* 일정 종료 시각 */}
+          <Text style={styles.date}>{end_time}</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -58,7 +54,16 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 10,
   },
-  /* 이름, 설명, 시각 공통 스타일 */
+  /* 일정 정보 View 스타일 */
+  scheduleInformation: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    width: 335,
+    marginLeft: 5,
+    borderTopEndRadius: 10,
+    borderBottomEndRadius: 10,
+  },
+  /* 이름, 설명, 시각 텍스트 공통 스타일 */
   scheduleText: {
     height: 50,
     marginHorizontal: 10,
