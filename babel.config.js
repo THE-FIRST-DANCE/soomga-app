7cb3a9ac0e399d4200a701f787e6b842e1c2f4b0
0gene68 <yeonghun8309@gmail.com>
2024-02-22 16:10:08 +0900

    feat: path alias 업데이트

--- babel.config.js
+++ babel.config.js
@@ -8,12 +8,18 @@ module.exports = function (api) {
         {
           root: ["./src"],
           alias: {
+            /* src 전체 디렉토리 */
             "@/*": "./src/*",
+            /* components 전체 디렉토리 */
             "@components/*": "./src/components/*",
+            /* main 디렉토리 */
             "@main/*": "./src/components/main/*",
-            "@recommend/*": "./src/components/main/recommend/*",
-            "@schedule/*": "./src/components/main/Schedule/*",
+            "@recommendMain/*": "./src/components/main/recommend/*",
+            "@scheduleMain/*": "./src/components/main/Schedule/*",
+            "@guideMain/*": "./src/components/main/guide/*",
+            /* profile 디렉토리 */
             "@profile/*": "./src/components/profile/*",
+            /* navigation 디렉토리 */
             "@navigation/*": "./src/navigation/*",
           },
         },
