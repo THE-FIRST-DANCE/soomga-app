ed1db25c982ed40b78f2cf5f36a0ea3eca653e18
0gene68 <yeonghun8309@gmail.com>
2024-02-26 20:08:50 +0900

    feat: 비밀번호 표시 여부 변경 & 회원가입 페이지 라우팅

--- src/components/profile/LogIn/LogIn.tsx
+++ src/components/profile/LogIn/LogIn.tsx
@@ -4,7 +4,15 @@ import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import InputText from "@profile/InputText";
 
 /* vector-icons */
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Feather } from "@expo/vector-icons";
+
+/* navigation */
+import { useNavigation } from "@react-navigation/native";
+import {
+  MyNavigationProp,
+  RootStackParamList,
+} from "@navigation/NavigationProps";
+import { useState } from "react";
 
 /* 소셜 로그인 부분 수평선 */
 function Hr() {
@@ -12,31 +20,61 @@ function Hr() {
 }
 
 function LogIn() {
+  /* navigation 추가 */
+  const navigation =
+    useNavigation<MyNavigationProp<keyof RootStackParamList>>();
+
+  /* 비밀번호 표시 여부 설정 */
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+
   return (
     <View style={{ marginHorizontal: 25 }}>
       {/* 제목, 부제목 */}
       <View>
-        <Text style={styles.title}>환영합니다!</Text>
-        <Text
-          style={styles.subtitle}
-        >{`여행의 시작, \nSOOMGA와 함께라면 \n당신만의 특별한 여행이 펼쳐집니다.`}</Text>
+        <Text style={styles.title}>어서오세요!</Text>
+        <Text style={styles.subtitle}>
+          여행의 시작, {"\n"}
+          <Text style={styles.soomgaText}>SOOMGA</Text>
+          <Text>와 함께라면</Text> {"\n"}
+          당신만의 특별한 여행이 펼쳐집니다.
+        </Text>
       </View>
       {/* 이메일, 비밀번호 입력창 */}
       <View style={styles.inputContainer}>
         <InputText title="이메일" placeholder="username@gmail.com" />
-        <InputText
-          title="비밀번호"
-          placeholder="비밀번호 입력"
-          style={{ marginTop: 20 }}
-        />
+        <View style={{ flexDirection: "row" }}>
+          <InputText
+            title="비밀번호"
+            placeholder="비밀번호 입력"
+            style={{ marginTop: 20 }}
+            isPasswordVisible={isPasswordVisible}
+          />
+          <TouchableOpacity
+            activeOpacity={1}
+            onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+            style={styles.visibleButton}
+          >
+            {isPasswordVisible ? (
+              <Feather name="eye" size={24} color="black" />
+            ) : (
+              <Feather name="eye-off" size={24} color="black" />
+            )}
+          </TouchableOpacity>
+        </View>
       </View>
       {/* 회원가입, 비밀번호 재설정 버튼 */}
       <View style={styles.signInButtonContainer}>
-        <TouchableOpacity style={{ marginTop: 10 }} activeOpacity={0.6}>
+        <TouchableOpacity
+          style={{ marginTop: 10 }}
+          activeOpacity={0.6}
+          onPress={() => {
+            navigation.navigate("회원가입");
+          }}
+        >
           <Text style={styles.signInText}>회원등록</Text>
         </TouchableOpacity>
         <TouchableOpacity style={{ marginTop: 10 }} activeOpacity={0.6}>
-          <Text style={styles.signInText}>비밀번호를 까먹었나요?</Text>
+          <Text style={styles.signInText}>비밀번호를 잊어버렸나요?</Text>
         </TouchableOpacity>
       </View>
       {/* 로그인 버튼 */}
@@ -83,6 +121,7 @@ const styles = StyleSheet.create({
   /* 제목 텍스트 스타일 */
   title: {
     fontSize: 40,
+    fontWeight: "700",
     marginTop: 100,
   },
   /* 부제목 스타일 */
@@ -90,10 +129,16 @@ const styles = StyleSheet.create({
     fontSize: 15,
     marginTop: 20,
   },
+  /* soomga 텍스트 스타일 */
+  soomgaText: {
+    color: "#DC2626",
+  },
   /* 이메일/비밀번호 입력 컨테이너 스타일 */
   inputContainer: {
     marginTop: 50,
   },
+  /* 비밀번호 표시 여부 버튼 스타일 */
+  visibleButton: { position: "absolute", right: 20, top: 60 },
   /* 회원가입, 비밀번호 재설정 버튼 컨테이너 스타일 */
   signInButtonContainer: {
     width: 320,
