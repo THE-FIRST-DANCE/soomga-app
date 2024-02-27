b5ba64e747ff2f52291d522f3c6f97e6bcc8f07c
0gene68 <yeonghun8309@gmail.com>
2024-02-27 15:13:42 +0900

    feat: 다음 페이지 이동 버튼 컴포넌트화

---
+++ src/components/profile/NextButton.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+/* vector-icons */
+import { AntDesign } from "@expo/vector-icons";
+
+/* props */
+type NextButtonType = {
+  style?: object;
+};
+
+function NextButton({ style }: NextButtonType) {
+  return (
+    <TouchableOpacity style={[styles.loginButton, style]} activeOpacity={0.6}>
+      <AntDesign name="arrowright" size={30} color="white" />
+    </TouchableOpacity>
+  );
+}
+
+export default NextButton;
+
+const styles = StyleSheet.create({
+  /* 로그인 버튼 스타일 */
+  loginButton: {
+    width: 60,
+    height: 60,
+    marginTop: 20,
+    marginRight: 10,
+    borderRadius: 100,
+    backgroundColor: "#DC2626",
+    elevation: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "flex-end",
+  },
+});
