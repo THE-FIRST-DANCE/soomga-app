954878ffaba831031c4b578b5f7a8bcd9a32142d
0gene68 <yeonghun8309@gmail.com>
2024-02-27 15:14:48 +0900

    feat: 비밀번호 입력 시 표시 여부 설정

--- src/components/profile/SignIn/SignIn.tsx
+++ src/components/profile/SignIn/SignIn.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import InputText from "@profile/InputText";
+
+/* vector-icons */
+import { Feather } from "@expo/vector-icons";
+import NextButton from "../NextButton";
 
 function SignIn() {
+  /* 비밀번호 표시 여부 설정 */
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+
   return (
     <View style={{ marginHorizontal: 25 }}>
       {/* 제목, 부제목 */}
@@ -13,7 +21,50 @@ function SignIn() {
           <Text>와 함께라면</Text> {"\n"}
           당신만의 특별한 여행이 펼쳐집니다.
         </Text>
+        {/* 이메일, 비밀번호 입력창 */}
+        <View style={styles.inputContainer}>
+          <InputText title="이메일" placeholder="username@gmail.com" />
+          <View style={{ flexDirection: "row" }}>
+            <InputText
+              title="비밀번호"
+              placeholder="비밀번호 입력"
+              style={{ marginTop: 20 }}
+              isPasswordVisible={isPasswordVisible}
+            />
+            <TouchableOpacity
+              activeOpacity={1}
+              onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+              style={styles.visibleButton}
+            >
+              {isPasswordVisible ? (
+                <Feather name="eye-off" size={24} color="black" />
+              ) : (
+                <Feather name="eye" size={24} color="black" />
+              )}
+            </TouchableOpacity>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <InputText
+              title="비밀번호 확인"
+              placeholder="비밀번호 입력"
+              style={{ marginTop: 20 }}
+              isPasswordVisible={isPasswordVisible}
+            />
+            <TouchableOpacity
+              activeOpacity={1}
+              onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+              style={styles.visibleButton}
+            >
+              {isPasswordVisible ? (
+                <Feather name="eye-off" size={24} color="black" />
+              ) : (
+                <Feather name="eye" size={24} color="black" />
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
+      <NextButton style={{ marginTop: 100 }} />
     </View>
   );
 }
@@ -36,4 +87,10 @@ const styles = StyleSheet.create({
   soomgaText: {
     color: "#DC2626",
   },
+  /* 이메일/비밀번호 입력 컨테이너 스타일 */
+  inputContainer: {
+    marginTop: 50,
+  },
+  /* 비밀번호 표시 여부 버튼 스타일 */
+  visibleButton: { position: "absolute", right: 20, top: 60 },
 });
