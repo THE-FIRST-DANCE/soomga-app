b465a8f7fa9b67673561639f8bcdc59663ead7de
hetame1 <poi8946@naver.com>
2024-02-28 21:22:40 +0900

    feat: 공통 color 값 추가

--- src/modules/Color.ts
+++ src/modules/Color.ts
@@ -1,6 +1,12 @@
 const Colors = {
   BLACK: "#000",
   WHITE: "#FFF",
+  PRIMARY: "#FFD766",
+  DANGER: "#FF0000",
+  GRAY_LIGHT: "#F5F5F5",
+  GRAY_MEDIUM: "#E0E0E0",
+  GRAY_DARK: "#757575",
+  GREEN: "#4CAF50",
 };
 
 export default Colors;
