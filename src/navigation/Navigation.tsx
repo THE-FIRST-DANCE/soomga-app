dd793915abb7b801a2f3ca1f3e6edfe222d86dfb
hetame1 <poi8946@naver.com>
2024-02-29 16:41:02 +0900

    feat: 리코일 사용 위해 RootRecoil 추가

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -15,6 +15,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Entypo, FontAwesome, FontAwesome6 } from "@expo/vector-icons";
 import { createStackNavigator } from "@react-navigation/stack";
 import PlanStack from "@/stacks/PlanStack";
+import { RecoilRoot } from "recoil";
 
 /* 하단 내비게이션 바 생성 */
 const Tab = createBottomTabNavigator();
@@ -24,78 +25,80 @@ const Stack = createStackNavigator();
 
 export default function Navigation() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: "#DC2626",
-          tabBarInactiveTintColor: "gray",
-          headerShown: false,
-          tabBarStyle: tabBarStyle,
-          tabBarLabelStyle: tabBarLabelStyle,
-          tabBarHideOnKeyboard: true,
-        }}
-        initialRouteName="Main"
-      >
-        <Tab.Screen
-          name="홈"
-          options={{
-            tabBarIcon: ({ color }) => (
-              <Entypo name="home" size={40} color={color} />
-            ),
+    <RecoilRoot>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarActiveTintColor: "#DC2626",
+            tabBarInactiveTintColor: "gray",
+            headerShown: false,
+            tabBarStyle: tabBarStyle,
+            tabBarLabelStyle: tabBarLabelStyle,
+            tabBarHideOnKeyboard: true,
           }}
+          initialRouteName="Main"
         >
-          {() => (
-            <Stack.Navigator>
-              <Stack.Screen
-                name="Home Page"
-                component={Main}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen name="태그 편집" component={TagEdit} />
-              <Stack.Screen name="여행 일정" component={Schedules} />
-            </Stack.Navigator>
-          )}
-        </Tab.Screen>
-        {/* 플랜 탭 */}
-        <Tab.Screen
-          name="플랜"
-          component={PlanStack}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name="paper-plane" size={33} color={color} />
-            ),
-          }}
-        />
-        {/* SOS 탭 */}
-        <Tab.Screen
-          name="SOS"
-          component={Sos}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <Entypo name="warning" size={35} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="채팅"
-          component={Chat}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <Entypo name="chat" size={35} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="프로필"
-          component={Profile}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <FontAwesome6 name="user-large" size={30} color={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+          <Tab.Screen
+            name="홈"
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Entypo name="home" size={40} color={color} />
+              ),
+            }}
+          >
+            {() => (
+              <Stack.Navigator>
+                <Stack.Screen
+                  name="Home Page"
+                  component={Main}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen name="태그 편집" component={TagEdit} />
+                <Stack.Screen name="여행 일정" component={Schedules} />
+              </Stack.Navigator>
+            )}
+          </Tab.Screen>
+          {/* 플랜 탭 */}
+          <Tab.Screen
+            name="플랜"
+            component={PlanStack}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <FontAwesome name="paper-plane" size={33} color={color} />
+              ),
+            }}
+          />
+          {/* SOS 탭 */}
+          <Tab.Screen
+            name="SOS"
+            component={Sos}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Entypo name="warning" size={35} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="채팅"
+            component={Chat}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Entypo name="chat" size={35} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="프로필"
+            component={Profile}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <FontAwesome6 name="user-large" size={30} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
   );
 }
 
