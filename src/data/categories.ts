ff3d42893b558ef873fd793f73f4ac728698d0ff
hetame1 <poi8946@naver.com>
2024-03-03 05:10:37 +0900

    feat: 카테고리 데이터

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+export const categories = [
+  { label: "추천", value: "all" },
+  { label: "명소", value: "tourist_attraction" },
+  { label: "음식점", value: "restaurant" },
+  { label: "카페", value: "cafe" },
+  { label: "숙소", value: "lodging" },
+];
