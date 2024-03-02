68400a8ea2667fb705c2d7c12b85fda2513f630b
hetame1 <poi8946@naver.com>
2024-03-03 05:10:10 +0900

    feat: 장소 선택 탭 작업
    장소 데이터 가져와서 나열하고 카테고리, 검색 기능 추가

---
+++ src/components/plan/PlaceSelectTab.tsx
@@ -0,0 +1,145 @@
+import Colors from "@/modules/Color";
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { categories } from "@/data/categories";
+import PlaceSelectTabItem from "./PlaceSelectTabItem";
+import { getPlaceApi } from "@/api/PlanApi";
+import { useQuery } from "@tanstack/react-query";
+import { PlaceData } from "@/interface/Plan";
+
+interface PlaceSelectTabProps {
+  region: string;
+}
+
+const PlaceSelectTab = ({ region }: PlaceSelectTabProps) => {
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const [places, setPlaces] = useState<PlaceData[]>([]);
+
+  const getPlace = () => {
+    return getPlaceApi(currentCategory.value, region);
+  };
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["places"],
+    queryFn: getPlace,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setPlaces(data);
+    }
+  }, [data]);
+
+  const filterPlaces = () => {
+    const filterPlaces =
+      currentCategory.value === "all"
+        ? data
+        : places.filter((place) => place.category === currentCategory.value);
+
+    const searchPlaces = filterPlaces.filter((place: PlaceData) =>
+      place.name.includes(searchKeyword)
+    );
+
+    setPlaces(searchPlaces);
+  };
+
+  useEffect(() => {
+    filterPlaces();
+  }, [searchKeyword, currentCategory]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.search}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="장소를 검색하세요"
+          value={searchKeyword}
+          onChangeText={(text) => setSearchKeyword(text)}
+        />
+        <Feather name="search" size={18} color={Colors.GRAY_DARK} />
+      </View>
+
+      <View style={styles.categories}>
+        {categories.map((category, index) => (
+          <TouchableOpacity
+            onPress={() => setCurrentCategory(category)}
+            key={index}
+            style={[
+              styles.category,
+              currentCategory === category && styles.selectedCategory,
+            ]}
+          >
+            <Text style={styles.categoryText}>{category.label}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <View style={styles.placeList}>
+        {places.length > 0 ? (
+          <FlatList
+            data={places}
+            keyExtractor={(item) => item?.id?.toString() || item.name}
+            renderItem={({ item }) => <PlaceSelectTabItem place={item} />}
+            ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
+          />
+        ) : (
+          <Text>장소가 없습니다.</Text>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default PlaceSelectTab;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 30,
+    marginTop: 20,
+  },
+  search: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderRadius: 5,
+    backgroundColor: Colors.WHITE,
+    padding: 15,
+  },
+  searchInput: {
+    borderRadius: 10,
+  },
+  categories: {
+    flexDirection: "row",
+    marginVertical: 10,
+    gap: 10,
+  },
+  category: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.GRAY_MEDIUM,
+  },
+  categoryText: {
+    fontSize: 16,
+    color: Colors.GRAY_DARK,
+  },
+  selectedCategory: {
+    backgroundColor: Colors.PRIMARY,
+    color: Colors.BLACK,
+  },
+  placeList: {
+    flex: 1,
+    marginTop: 10,
+  },
+});
