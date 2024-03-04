f3c647692a119c53b61af50d42115d631ef4938d
0gene68 <yeonghun8309@gmail.com>
2024-03-04 09:12:37 +0900

    refactor: 태그 컴포넌트화

--- src/components/main/Tags.tsx
+++ src/components/main/Tags.tsx
@@ -1,25 +1,15 @@
-import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Dimensions, ScrollView } from "react-native";
 import { useState } from "react";
 
 /* Navigation */
+import { useNavigation } from "@react-navigation/native";
 import {
   MyNavigationProp,
   RootStackParamList,
 } from "@navigation/NavigationProps";
 
-import {
-  StyleSheet,
-  Text,
-  Dimensions,
-  ScrollView,
-  Pressable,
-} from "react-native";
-
-/* props */
-type Tag = {
-  id: number;
-  name: string;
-};
+/* components */
+import Tag, { TagType } from "@main/Tag";
 
 function Tags() {
   /* 임시 데이터 */
@@ -36,7 +26,7 @@ function Tags() {
   const lastIndex = tags.length;
 
   /* 전체 태그 상태 관리 */
-  const [allTags, setAllTags] = useState<Tag[]>([
+  const [allTags, setAllTags] = useState<TagType[]>([
     ...tags,
     { id: lastIndex + 1, name: "..." },
   ]);
@@ -52,17 +42,15 @@ function Tags() {
       style={styles.container}
     >
       {allTags.map((tag, index) => (
-        <Pressable
-          key={tag.id}
-          style={styles.tag}
+        <Tag
+          key={index + 1}
+          name={tag.name}
           onPress={() => {
             if (index === allTags.length - 1) {
               navigation.navigate("태그 편집");
             }
           }}
-        >
-          <Text>{tag.name}</Text>
-        </Pressable>
+        />
       ))}
     </ScrollView>
   );
@@ -78,18 +66,6 @@ const styles = StyleSheet.create({
     height: 50,
     marginHorizontal: 10,
     marginTop: 5,
-  },
-  /* 각 태그 스타일 */
-  tag: {
-    borderColor: "#DC2626",
-    borderWidth: 1,
-    borderRadius: 20,
-    marginHorizontal: 3,
-    marginVertical: 5,
-    padding: 7,
-    height: 35,
-    backgroundColor: "white",
-    elevation: 5,
   },
 });
 
