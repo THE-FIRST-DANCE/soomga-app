f5e1f337952affabb19bebd9b1c2255fb55143a2
0gene68 <yeonghun8309@gmail.com>
2024-03-04 20:07:21 +0900

    refactor: 컴포넌트 이름 변경(TagComponent → Category)

--- src/components/profile/SignIn/TagsSelect.tsx
+++ src/components/profile/SignIn/TagsSelect.tsx
@@ -5,10 +5,11 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
+import { useState } from "react";
 
 /* components */
-import TagComponent from "@signIn/Category";
-import { tags } from "@/data/tags";
+import Category from "@signIn/Category";
+import { tags } from "@data/tags";
 
 function TagsSelect() {
   return (
@@ -22,26 +23,25 @@ function TagsSelect() {
         당신만의 특별한 여행이 펼쳐집니다.
       </Text>
       <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <Text style={styles.tagSelectTitle}>태그 선택</Text>
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <Text style={styles.tagSelectTitle}>태그 선택</Text>
+        </View>
         <TouchableOpacity style={{ marginTop: 20, marginRight: 10 }}>
-          <Text style={{ fontSize: 20, color: "#DC2626" }}>완료</Text>
+          <Text style={styles.finishButton}>완료</Text>
         </TouchableOpacity>
       </View>
-      <ScrollView
-        style={{ height: 460, marginTop: 10 }}
-        showsVerticalScrollIndicator={false}
-      >
-        <TagComponent label="MBTI" category={tags.mbti} />
-        <TagComponent label="엔터테인먼트" category={tags.entertainment} />
-        <TagComponent label="취미 & 관심사" category={tags.hobby} />
-        <TagComponent label="캐릭터" category={tags.character} />
-        <TagComponent label="라이프스타일" category={tags.lifestyle} />
-        <TagComponent label="아트 & 뷰티" category={tags.artBeauty} />
-        <TagComponent label="음식" category={tags.foods} />
-        <TagComponent label="스포츠" category={tags.sports} />
-        <TagComponent label="웰빙" category={tags.wellbeing} />
-        <TagComponent label="재테크" category={tags.investment} />
-        <TagComponent label="커리어 & 전공" category={tags.career} />
+      <ScrollView style={{ height: 460 }} showsVerticalScrollIndicator={false}>
+        <Category label="MBTI" category={tags.mbti} />
+        <Category label="엔터테인먼트" category={tags.entertainment} />
+        <Category label="취미 & 관심사" category={tags.hobby} />
+        <Category label="캐릭터" category={tags.character} />
+        <Category label="라이프스타일" category={tags.lifestyle} />
+        <Category label="아트 & 뷰티" category={tags.artBeauty} />
+        <Category label="음식" category={tags.foods} />
+        <Category label="스포츠" category={tags.sports} />
+        <Category label="웰빙" category={tags.wellbeing} />
+        <Category label="재테크" category={tags.investment} />
+        <Category label="커리어 & 전공" category={tags.career} />
       </ScrollView>
     </View>
   );
@@ -67,4 +67,6 @@ const styles = StyleSheet.create({
   },
   /* 태그 선택 제목 스타일 */
   tagSelectTitle: { fontSize: 30, fontWeight: "700", marginTop: 10 },
+  /* 완료 버튼 스타일 */
+  finishButton: { fontSize: 20, color: "#DC2626" },
 });
