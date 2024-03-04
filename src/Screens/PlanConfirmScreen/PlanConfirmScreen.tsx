4d80c8a38577be930d53facddaf1971655f37259
hetame1 <poi8946@naver.com>
2024-03-04 20:05:04 +0900

    feat: 경로 완료 페이지 구조만

---
+++ src/Screens/PlanConfirmScreen/PlanConfirmScreen.tsx
@@ -0,0 +1,13 @@
+import Screen from "@/components/Screen";
+import React from "react";
+import { View } from "react-native";
+
+const PlanConfirmScreen = () => {
+  return (
+    <Screen title="일정 확인">
+      <View></View>
+    </Screen>
+  );
+};
+
+export default PlanConfirmScreen;
