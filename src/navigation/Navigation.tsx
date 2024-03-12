21b4d4a5332ddafbbd0c0d12bb03fe23e4b9c302
0gene68 <yeonghun8309@gmail.com>
2024-03-12 20:31:32 +0900

    refactor: 각 화면 파일 위치 변경

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,11 +1,7 @@
 /* Pages */
-import Main from "@main/Main";
-import Plan from "@components/Plan";
-import Sos from "@components/Sos";
-import Chat from "@components/Chat";
-import Profile from "@profile/Profile";
-import TagEdit from "@profile/TagEdit";
-import Schedules from "@profile/Schedules";
+import Sos from "@/Screens/SosScreen/Sos";
+import Chat from "@/Screens/ChatScreen/Chat";
+import Profile from "@/Screens/ProfileScreen/Profile";
 
 /* 화면 전환 */
 import { NavigationContainer } from "@react-navigation/native";
@@ -13,8 +9,13 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 /* vector-icons */
 import { Entypo, FontAwesome, FontAwesome6 } from "@expo/vector-icons";
+
+/* Stack Navigator */
 import { createStackNavigator } from "@react-navigation/stack";
+
+/* stacks */
 import PlanStack from "@/stacks/PlanStack";
+import HomeStack from "@/stacks/HomeStack";
 
 /* 하단 내비게이션 바 생성 */
 const Tab = createBottomTabNavigator();
@@ -38,24 +39,13 @@ export default function Navigation() {
       >
         <Tab.Screen
           name="홈"
+          component={HomeStack}
           options={{
             tabBarIcon: ({ color }) => (
               <Entypo name="home" size={40} color={color} />
             ),
           }}
-        >
-          {() => (
-            <Stack.Navigator>
-              <Stack.Screen
-                name="Home Page"
-                component={Main}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen name="태그 편집" component={TagEdit} />
-              <Stack.Screen name="여행 일정" component={Schedules} />
-            </Stack.Navigator>
-          )}
-        </Tab.Screen>
+        />
         {/* 플랜 탭 */}
         <Tab.Screen
           name="플랜"
