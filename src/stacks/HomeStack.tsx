6c8a68bb2eef66bc62a290e3676ac9087ed03d2f
0gene68 <yeonghun8309@gmail.com>
2024-03-12 20:33:16 +0900

    refactor: 홈 화면 Stack 추가

---
+++ src/stacks/HomeStack.tsx
@@ -0,0 +1,23 @@
+import Main from "@/Screens/MainScreen/Main";
+import Schedules from "@/Screens/ProfileScreen/Schedules";
+import TagEdit from "@/components/profile/TagEdit";
+
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home Page"
+        component={Main}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="태그 편집" component={TagEdit} />
+      <Stack.Screen name="여행 일정" component={Schedules} />
+    </Stack.Navigator>
+  );
+}
+
+export default HomeStack;
