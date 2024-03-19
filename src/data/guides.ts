bb21b4faaf698ee64b0af573e20c55040341a995
0gene68 <yeonghun8309@gmail.com>
2024-03-19 09:48:34 +0900

    refactor: 가이드 생년월일 날짜 형식 문자열로 변경

--- src/data/guides.ts
+++ src/data/guides.ts
@@ -4,7 +4,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/QmbIdfT-NHnIb3KUMDX_oktsghL6h53bkOE2ug_acbotkWOIFSNmr5MsG6yW-sskffl_ExjPcygA2bAphb2IoD0bytOvp-HX0HE1gWWsNoLH3F02W61y27LbKwbvOYw1wU8ZhP4UiXY7ATdqGT3C9g.webp",
     name: "민지",
-    birthDate: new Date("2004-05-07T00:00:00.000Z"),
+    birthDate: new Date("2004-05-07T00:00:00.000Z").toISOString(),
     gender: "F",
     description: "I'm Minji!",
     stars: 4.8,
@@ -26,7 +26,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/Pij2oJ3Ktf7BX5sBHeWROiieSUgkS1a5Tpzt8kY3zUXm0S_-uCZFFEXaimbYTW_UxkYdGinfdyx2duJEKYSQb7-XnGKtj0Bi_B8QMT3FsGjpZ0xd_buHk_5QmNuuCV3-2STZ5kdvKTuUTDgg-6IYNw.webp",
     name: "하니",
-    birthDate: new Date("2004-10-06T00:00:00.000Z"),
+    birthDate: new Date("2004-10-06T00:00:00.000Z").toISOString(),
     gender: "M",
     description: "I'm Hanni!",
     stars: 4.3,
@@ -48,7 +48,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/jLt6JzGh5ubLdwp7XZNB_Ju8KdOrN9rgyByfY9xgpy0oZOcYLKUTF0afGlW3Gxw99brC4yphS8btlym5u1VTlnFDrrQWaaULysb_Yh8gtP6YjrijrvNIYTbDGNquUaS-qdYsECtTPdNdR8-qZD1KVg.webp",
     name: "다니엘",
-    birthDate: new Date("2005-04-11T00:00:00.000Z"),
+    birthDate: new Date("2005-04-11T00:00:00.000Z").toISOString(),
     gender: "F",
     description: "I'm Danielle!",
     stars: 4.6,
@@ -70,7 +70,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/C-V89EwsWR3gF76iKVr67Ed6pGfUWskALPcDXQoGZ1pGJn9vAQLhE90p4A7j4liFa8QBVS81Xy_codemBGOtcOm-S6_c7m336h5uGcQsHT1rEgDS3c_e9fN6VlEzjrJkNUnlxekbEjyjeCVzlZib-Q.webp",
     name: "해린",
-    birthDate: new Date("2006-05-15T00:00:00.000Z"),
+    birthDate: new Date("2006-05-15T00:00:00.000Z").toISOString(),
     gender: "M",
     description: "I'm Haerin!",
     stars: 4.1,
@@ -92,7 +92,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/vuyQGsxqEQKS6fzhE7psuDrup0T2cN5frkg_Dnai9606xKDoKuEm6GSXbH4GpfGufHSC3plqaCQG9LEXzsI20zMhkztS7hQTmaGBdVCFCL_zuyMgGBB4c96ivf0L3yrr2yhqAbdKrlor-lTAk4P6Tg.webp",
     name: "혜인",
-    birthDate: new Date("2008-04-21T00:00:00.000Z"),
+    birthDate: new Date("2008-04-21T00:00:00.000Z").toISOString(),
     gender: "F",
     description: "I'm Hyein!",
     stars: 4.8,
@@ -114,7 +114,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/QmeaTW3wq13-8CWl-vwJ1Vo8PmlbS1FSITLvnywB-HmwRfRl3qh7-62nSKxLC3kjlDayMIwrCTi6mj_RXtCTkTchNDpp2bnjgeFFAgN0lcbJPpHYex2dkMykR1zJ92lLKg1wW0jgKdWD-XQ71YyIUA.webp",
     name: "가을",
-    birthDate: new Date("2002-09-24T00:00:00.000Z"),
+    birthDate: new Date("2002-09-24T00:00:00.000Z").toISOString(),
     gender: "F",
     description: "I'm Armamenter!",
     stars: 4.6,
@@ -136,7 +136,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/u0gMjUqIAnuuAPNMsUYRn6RH3Amq5uH866oV6vnOo6CyWNUMgXUlWoX8Hc8jJkf_00-9-Lgs6cdJ4s7bzIHwBlY861MAA5BVNGCN_KS7PQwhIAFXWN6o2jAuKobGIBkXPCpk3fiohd_6Ng1ziavBDg.webp",
     name: "안유진",
-    birthDate: new Date("2003-09-01T00:00:00.000Z"),
+    birthDate: new Date("2003-09-01T00:00:00.000Z").toISOString(),
     gender: "M",
     description: "I'm Yujin!",
     stars: 4.3,
@@ -159,7 +159,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/0loxrECDHdD_JISekO7nNFF2k2_uq627YTFaOkYVI57hdNaRdsnP2hst2034-rXfWzDh7STEF1jCyWFb2Ebr1jOPmxeApraQbJA_0APutze5d1tYB5TEuexiAfWCSiEKQetEzYxFTRFqVgf1ucKcdg.webp",
     name: "레이",
-    birthDate: new Date("2004-02-03T00:00:00.000Z"),
+    birthDate: new Date("2004-02-03T00:00:00.000Z").toISOString(),
     gender: "M",
     description: "I'm Rei!",
     stars: 4.1,
@@ -180,7 +180,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/9yNnY4ujxgwAO_j0Gt1aXCFm6O4bXKvpUzMF_rSYeHCZF6r9tNJCZ350bhhEkbblEIX_o7EM4G6-OAHVF7dR1uqMxQSn-VQM2KImq_uZCC9vRXBp5B6T3QKnBOGAabDN1N4dKc4sVKofPnzpxJegrg.webp",
     name: "리즈",
-    birthDate: new Date("2004-11-21T00:00:00.000Z"),
+    birthDate: new Date("2004-11-21T00:00:00.000Z").toISOString(),
     gender: "M",
     description: "I'm Liz!",
     stars: 4.3,
@@ -202,7 +202,7 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/vWnlonT4HJr6W3qNlPwyxH1WwGZk-7U0qp8WTNx_UHdBhkz7LmgS6nourfuLagblRjqTFQHoR0GIiKz7Y5hV_Vmn8G31LMAUhdh7ID0jGl249ys68DCfvgOUMLeKpxZt-QVdDpRyWWm9nxC_sLcE8A.webp",
     name: "이서",
-    birthDate: new Date("2007-02-21T00:00:00.000Z"),
+    birthDate: new Date("2007-02-21T00:00:00.000Z").toISOString(),
     gender: "F",
     description: "I'm Leeseo!",
     stars: 4.6,
