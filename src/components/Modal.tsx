5a7c3f04846590be7e389ca463163e9b91c708e5
hetame1 <poi8946@naver.com>
2024-03-25 09:41:25 +0900

    feat: 글로벌 modal 추가

---
+++ src/components/Modal.tsx
@@ -0,0 +1,77 @@
+import Colors from "@/modules/Color";
+import { Modal, Pressable, StyleSheet, View } from "react-native";
+
+interface ModalProps {
+  visible: boolean;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  children: React.ReactNode;
+  type?: "full" | "bottom" | "center";
+}
+
+const GlobalModal = ({ visible, setVisible, children, type }: ModalProps) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={visible}
+      onRequestClose={() => setVisible(false)}
+    >
+      <Pressable
+        style={styles.modalContainer}
+        onPress={() => setVisible(false)}
+      >
+        <View
+          style={
+            type === "full"
+              ? styles.modalFull
+              : type === "bottom"
+              ? styles.modalBottom
+              : styles.modalCenter
+          }
+        >
+          {children}
+        </View>
+      </Pressable>
+    </Modal>
+  );
+};
+
+export default GlobalModal;
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalFull: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: Colors.WHITE,
+    padding: 35,
+  },
+  modalBottom: {
+    position: "absolute",
+    bottom: "5%",
+    width: "95%",
+    backgroundColor: Colors.WHITE,
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    shadowColor: Colors.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    borderRadius: 20,
+  },
+  modalCenter: {
+    width: "80%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.WHITE,
+    padding: 20,
+    borderRadius: 15,
+  },
+});
