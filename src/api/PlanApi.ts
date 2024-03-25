5db9c111a3cee5ba74dd1494bb9e434deda8a0a3
hetame1 <poi8946@naver.com>
2024-03-25 09:41:11 +0900

    feat: 이벤트 crud api 추가

--- src/api/PlanApi.ts
+++ src/api/PlanApi.ts
@@ -2,12 +2,13 @@ import {
   GooglePlaceResponse,
   PlaceData,
   PlanListConfirm,
+  Plans,
 } from "@/interface/Plan";
 import axios from "axios";
 import { EXPO_PUBLIC_KAKAO_API } from "@env";
 
 export const api = axios.create({
-  baseURL: "http://192.168.0.20:3000/",
+  baseURL: "http://localhost:3000/",
   withCredentials: true,
 });
 
@@ -118,6 +119,12 @@ export const getPlanList = async (authorId: number) => {
 
 export const getPlanById = async (planId: number) => {
   const response = await api.get(`plans/${planId}`);
+
+  return response.data;
+};
+
+export const getPlanByUserId = async (userId: number): Promise<Plans[]> => {
+  const response = await api.get(`plans/user/${userId}`);
 
   return response.data;
 };
