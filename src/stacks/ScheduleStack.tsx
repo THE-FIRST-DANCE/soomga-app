f0fcb1f58844680a75b54deda66aa33145d3c48b
hetame1 <poi8946@naver.com>
2024-03-25 09:42:56 +0900

    feat: 이벤트 생성, 상세보기 스크린 연결

--- src/stacks/ScheduleStack.tsx
+++ src/stacks/ScheduleStack.tsx
@@ -1,8 +1,18 @@
+import ScheduleCreateScreen from "@/Screens/ScheduleScreen/ScheduleCreateScreen";
+import ScheduleDetailScreen from "@/Screens/ScheduleScreen/ScheduleDetailScreen";
 import ScheduleScreen from "@/Screens/ScheduleScreen/ScheduleScreen";
+import { EventData } from "@/interface/Event";
 import { createStackNavigator } from "@react-navigation/stack";
 
 export type ScheduleStackParamList = {
   ScheduleScreen: undefined;
+  ScheduleCreateScreen: {
+    date: string;
+    editEvent?: EventData;
+  };
+  ScheduleDetailScreen: {
+    event: EventData;
+  };
 };
 
 const Stack = createStackNavigator<ScheduleStackParamList>();
@@ -15,6 +25,14 @@ export default function ScheduleStack() {
       }}
     >
       <Stack.Screen name="ScheduleScreen" component={ScheduleScreen} />
+      <Stack.Screen
+        name="ScheduleCreateScreen"
+        component={ScheduleCreateScreen}
+      />
+      <Stack.Screen
+        name="ScheduleDetailScreen"
+        component={ScheduleDetailScreen}
+      />
     </Stack.Navigator>
   );
 }
