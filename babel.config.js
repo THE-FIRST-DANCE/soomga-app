d1f41ab1dc8d29c68e9bc5c31ba081aad178f688
0gene68 <yeonghun8309@gmail.com>
2024-03-27 22:17:18 +0900

    refactor: path alias 재정의

--- babel.config.js
+++ babel.config.js
@@ -10,19 +10,37 @@ module.exports = function (api) {
           alias: {
             /* src 전체 디렉토리 */
             "@/*": "./src/*",
+            /* api 디렉토리 */
+            "@api/*": "./src/api/*",
+            /* assets 디렉토리 */
+            "@assets/*": "./src/assets/*",
             /* components 전체 디렉토리 */
             "@components/*": "./src/components/*",
-            /* main 디렉토리 */
+            /* components/main 디렉토리 */
             "@main/*": "./src/components/main/*",
             "@recommendMain/*": "./src/components/main/recommend/*",
             "@guideMain/*": "./src/components/main/recommend/Guide/*",
             "@placeMain/*": "./src/components/main/recommend/Place/*",
             "@regionMain/*": "./src/components/main/recommend/Region/*",
             "@scheduleMain/*": "./src/components/main/Schedule/*",
-            /* profile 디렉토리 */
-            "@profile/*": "./src/components/profile/*",
+            /* data 디렉토리 */
+            "@data/*": "./src/data/*",
+            /* hooks 디렉토리 */
+            "@hooks/*": "./src/hooks/*",
+            /* interface 디렉토리 */
+            "@interface/*": "./src/interface/*",
+            /* modules 디렉토리 */
+            "@modules/*": "./src/modules/*",
             /* navigation 디렉토리 */
             "@navigation/*": "./src/navigation/*",
+            /* Screens 디렉토리 */
+            "@screens/*": "./src/screens/*",
+            /* stacks 디렉토리 */
+            "@stacks/*": "./src/stacks/*",
+            /* state 디렉토리 */
+            "@state/*": "./src/state/*",
+            /* profile 디렉토리 */
+            "@profile/*": "./src/components/profile/*",
           },
         },
       ],
