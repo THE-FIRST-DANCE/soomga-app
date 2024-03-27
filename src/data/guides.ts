5df948b3e6529907c0421138ca124b81abc7308b
0gene68 <yeonghun8309@gmail.com>
2024-03-27 15:40:51 +0900

    chore: 온도, 가이드 횟수 랜덤 숫자에서 정해진 값으로 변경

--- src/data/guides.ts
+++ src/data/guides.ts
@@ -2,11 +2,13 @@ export type GuideType = {
   id: number;
   photo: string;
   name: string;
+  region: string;
   birthDate: string;
   gender: string;
   description: string;
   stars: number;
-  region: string;
+  temp: number;
+  guideCount: number;
   language: string[];
   tags: {
     id: number;
@@ -20,11 +22,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/QmbIdfT-NHnIb3KUMDX_oktsghL6h53bkOE2ug_acbotkWOIFSNmr5MsG6yW-sskffl_ExjPcygA2bAphb2IoD0bytOvp-HX0HE1gWWsNoLH3F02W61y27LbKwbvOYw1wU8ZhP4UiXY7ATdqGT3C9g.webp",
     name: "민지",
+    region: "강원도",
     birthDate: new Date("2004-05-07T00:00:00.000Z").toISOString(),
-    gender: "F",
+    gender: "여자",
     description: "I'm Minji!",
     stars: 4.8,
-    region: "강원도",
+    temp: 92,
+    guideCount: 80,
     language: ["한국어", "English"],
     tags: [
       { id: 1, name: "#ESTJ" },
@@ -43,11 +47,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/Pij2oJ3Ktf7BX5sBHeWROiieSUgkS1a5Tpzt8kY3zUXm0S_-uCZFFEXaimbYTW_UxkYdGinfdyx2duJEKYSQb7-XnGKtj0Bi_B8QMT3FsGjpZ0xd_buHk_5QmNuuCV3-2STZ5kdvKTuUTDgg-6IYNw.webp",
     name: "하니",
+    region: "서울특별시",
     birthDate: new Date("2004-10-06T00:00:00.000Z").toISOString(),
-    gender: "M",
+    gender: "남자",
     description: "I'm Hanni!",
     stars: 4.3,
-    region: "서울특별시",
+    temp: 60,
+    guideCount: 40,
     language: ["한국어", "English", "Vietnamese"],
     tags: [
       { id: 1, name: "#INFP" },
@@ -66,11 +72,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/jLt6JzGh5ubLdwp7XZNB_Ju8KdOrN9rgyByfY9xgpy0oZOcYLKUTF0afGlW3Gxw99brC4yphS8btlym5u1VTlnFDrrQWaaULysb_Yh8gtP6YjrijrvNIYTbDGNquUaS-qdYsECtTPdNdR8-qZD1KVg.webp",
     name: "다니엘",
+    region: "경기도",
     birthDate: new Date("2005-04-11T00:00:00.000Z").toISOString(),
-    gender: "F",
+    gender: "여자",
     description: "I'm Danielle!",
     stars: 4.6,
-    region: "경기도",
+    temp: 77,
+    guideCount: 77,
     language: ["한국어", "English"],
     tags: [
       { id: 1, name: "#ENFP" },
@@ -89,11 +97,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/C-V89EwsWR3gF76iKVr67Ed6pGfUWskALPcDXQoGZ1pGJn9vAQLhE90p4A7j4liFa8QBVS81Xy_codemBGOtcOm-S6_c7m336h5uGcQsHT1rEgDS3c_e9fN6VlEzjrJkNUnlxekbEjyjeCVzlZib-Q.webp",
     name: "해린",
+    region: "경기도",
     birthDate: new Date("2006-05-15T00:00:00.000Z").toISOString(),
-    gender: "M",
+    gender: "남자",
     description: "I'm Haerin!",
     stars: 4.1,
-    region: "경기도",
+    temp: 65,
+    guideCount: 40,
     language: ["한국어", "English"],
     tags: [
       { id: 1, name: "#INTJ" },
@@ -112,11 +122,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/vuyQGsxqEQKS6fzhE7psuDrup0T2cN5frkg_Dnai9606xKDoKuEm6GSXbH4GpfGufHSC3plqaCQG9LEXzsI20zMhkztS7hQTmaGBdVCFCL_zuyMgGBB4c96ivf0L3yrr2yhqAbdKrlor-lTAk4P6Tg.webp",
     name: "혜인",
+    region: "인천광역시",
     birthDate: new Date("2008-04-21T00:00:00.000Z").toISOString(),
-    gender: "F",
+    gender: "여자",
     description: "I'm Hyein!",
     stars: 4.8,
-    region: "인천광역시",
+    temp: 75,
+    guideCount: 80,
     language: ["한국어", "English"],
     tags: [
       { id: 1, name: "#INFP" },
@@ -135,11 +147,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/QmeaTW3wq13-8CWl-vwJ1Vo8PmlbS1FSITLvnywB-HmwRfRl3qh7-62nSKxLC3kjlDayMIwrCTi6mj_RXtCTkTchNDpp2bnjgeFFAgN0lcbJPpHYex2dkMykR1zJ92lLKg1wW0jgKdWD-XQ71YyIUA.webp",
     name: "가을",
+    region: "인천광역시",
     birthDate: new Date("2002-09-24T00:00:00.000Z").toISOString(),
-    gender: "F",
+    gender: "여자",
     description: "I'm Gaeul!",
     stars: 4.6,
-    region: "인천광역시",
+    temp: 45,
+    guideCount: 20,
     language: ["한국어", "English"],
     tags: [
       { id: 1, name: "#ISTJ" },
@@ -158,11 +172,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/u0gMjUqIAnuuAPNMsUYRn6RH3Amq5uH866oV6vnOo6CyWNUMgXUlWoX8Hc8jJkf_00-9-Lgs6cdJ4s7bzIHwBlY861MAA5BVNGCN_KS7PQwhIAFXWN6o2jAuKobGIBkXPCpk3fiohd_6Ng1ziavBDg.webp",
     name: "안유진",
+    region: "대전광역시",
     birthDate: new Date("2003-09-01T00:00:00.000Z").toISOString(),
-    gender: "M",
+    gender: "남자",
     description: "I'm Yujin!",
     stars: 4.3,
-    region: "대전광역시",
+    temp: 50,
+    guideCount: 50,
     language: ["한국어", "English"],
     tags: [
       { id: 1, name: "#ESTP" },
@@ -181,11 +197,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/0loxrECDHdD_JISekO7nNFF2k2_uq627YTFaOkYVI57hdNaRdsnP2hst2034-rXfWzDh7STEF1jCyWFb2Ebr1jOPmxeApraQbJA_0APutze5d1tYB5TEuexiAfWCSiEKQetEzYxFTRFqVgf1ucKcdg.webp",
     name: "레이",
+    region: "서울특별시",
     birthDate: new Date("2004-02-03T00:00:00.000Z").toISOString(),
-    gender: "M",
+    gender: "남자",
     description: "I'm Rei!",
     stars: 4.1,
-    region: "서울특별시",
+    temp: 34,
+    guideCount: 30,
     language: ["한국어", "日本語", "English"],
     tags: [
       { id: 1, name: "#INFJ" },
@@ -204,11 +222,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/9yNnY4ujxgwAO_j0Gt1aXCFm6O4bXKvpUzMF_rSYeHCZF6r9tNJCZ350bhhEkbblEIX_o7EM4G6-OAHVF7dR1uqMxQSn-VQM2KImq_uZCC9vRXBp5B6T3QKnBOGAabDN1N4dKc4sVKofPnzpxJegrg.webp",
     name: "리즈",
+    region: "제주특별자치도",
     birthDate: new Date("2004-11-21T00:00:00.000Z").toISOString(),
-    gender: "M",
+    gender: "남자",
     description: "I'm Liz!",
     stars: 4.3,
-    region: "제주특별자치도",
+    temp: 45,
+    guideCount: 40,
     language: ["한국어", "English"],
     tags: [
       { id: 1, name: "#INFJ" },
@@ -227,11 +247,13 @@ export const guides = [
     photo:
       "https://i.namu.wiki/i/vWnlonT4HJr6W3qNlPwyxH1WwGZk-7U0qp8WTNx_UHdBhkz7LmgS6nourfuLagblRjqTFQHoR0GIiKz7Y5hV_Vmn8G31LMAUhdh7ID0jGl249ys68DCfvgOUMLeKpxZt-QVdDpRyWWm9nxC_sLcE8A.webp",
     name: "이서",
+    region: "서울특별시",
     birthDate: new Date("2007-02-21T00:00:00.000Z").toISOString(),
-    gender: "F",
+    gender: "여자",
     description: "I'm Leeseo!",
     stars: 4.6,
-    region: "서울특별시",
+    temp: 88,
+    guideCount: 50,
     language: ["한국어", "English"],
     tags: [
       { id: 1, name: "#INFJ" },
