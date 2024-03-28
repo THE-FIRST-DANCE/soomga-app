effbe5fe80538d73c5a185d9346f5e606ec4d75f
hetame1 <poi8946@naver.com>
2024-03-28 15:05:15 +0900

    feat: Sos 글 저장, 로드 api 추가

---
+++ src/api/SosApi.ts
@@ -0,0 +1,19 @@
+import { api } from "./PlanApi";
+
+interface SosContent {
+  content: string;
+  lat: number;
+  lng: number;
+}
+
+export const addSos = async (createSosDto: SosContent) => {
+  const response = await api.post("/sos", createSosDto);
+
+  return response.data;
+};
+
+export const getSos = async (page: number) => {
+  const response = await api.get(`/sos/all?page=${page}`);
+
+  return response.data;
+};
