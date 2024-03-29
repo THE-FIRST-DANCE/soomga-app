c1523dfad74a7720396569edaa90af7bb1740017
0gene68 <yeonghun8309@gmail.com>
2024-03-29 11:34:25 +0900

    feat: 언어, 성별, 자격증, 평점 선택 여부 전역 상태 관리

--- src/state/store/GuideFilterRecoil.tsx
+++ src/state/store/GuideFilterRecoil.tsx
@@ -1,31 +1,74 @@
 import { atom } from "recoil";
 
-export const ageRangeState = atom<number[]>({
-  key: "ageRangeState",
+// 나이 선택 범위
+export const AgeRangeState = atom<number[]>({
+  key: "AgeRangeState",
   default: [10, 70],
 });
 
-export const tempRangeState = atom<number[]>({
-  key: "tempRangeState",
+// 온도 선택 범위
+export const TempRangeState = atom<number[]>({
+  key: "TempRangeState",
   default: [0, 100],
 });
 
-export const guideCountRangeState = atom<number[]>({
-  key: "guideCountRangeState",
+// 가이드 횟수 선택 범위
+export const GuideCountRangeState = atom<number[]>({
+  key: "GuideCountRangeState",
   default: [0, 100],
 });
 
-export const langsState = atom<string[]>({
-  key: "langsState",
+// 선택한 언어
+export const LangsState = atom<string[]>({
+  key: "LangsState",
   default: [],
 });
 
-export const gendersState = atom<string[]>({
-  key: "gendersState",
+export const IsLangsSelectedState = atom<boolean[]>({
+  key: "IsLangsSelectedState",
+  default: [true, false, false, false],
+});
+
+// 선택한 성별
+export const GendersState = atom<string[]>({
+  key: "GendersState",
   default: [],
 });
 
-export const selectedRatingState = atom<number[]>({
-  key: "selectedRatingState",
+export const IsGendersSelectedState = atom<boolean[]>({
+  key: "IsGendersSelectedState",
+  default: [true, false, false],
+});
+
+// 선택한 일본어 자격증
+export const JpnCertificateState = atom<string[]>({
+  key: "JpnCertificateState",
   default: [],
+});
+
+export const IsJpnCertificateSelectedState = atom<boolean[]>({
+  key: "IsJpnCertificateSelectedState",
+  default: [true, false, false, false, false, false],
+});
+
+// 선택한 영어 자격증
+export const EngCertificateState = atom<string[]>({
+  key: "EngCertificateState",
+  default: [],
+});
+
+export const IsEngCertificateSelectedState = atom<boolean[]>({
+  key: "IsEngCertificateSelectedState",
+  default: [true, false, false, false, false, false],
+});
+
+// 선택한 별점
+export const SelectedRatingState = atom<number[]>({
+  key: "SelectedRatingState",
+  default: [],
+});
+
+export const IsRatingSelectedState = atom<boolean[]>({
+  key: "IsRatingSelectedState",
+  default: [false, false, false, false, false],
 });
