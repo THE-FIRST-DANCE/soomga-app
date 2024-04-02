2648c2f30e6c1380c6fbf89997d5950834e7d88f
0gene68 <yeonghun8309@gmail.com>
2024-04-02 10:21:36 +0900

    feat: 상세 페이지 탭바 생성

--- src/Screens/GuideScreen/GuideDetailScreen.tsx
+++ src/Screens/GuideScreen/GuideDetailScreen.tsx
@@ -1,48 +1,117 @@
 import Screen from "@/components/Screen";
 import { GuideStackParamList } from "@/stacks/GuideStack";
 import { RouteProp, useRoute } from "@react-navigation/native";
-import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  Pressable,
+} from "react-native";
 import { calculateAgeRange, TempBar } from "@/components/guide/GuideListInfo";
-import Colors from "@/modules/Color";
+import { useRef, useState } from "react";
 
 function GuideDetailScreen() {
   const route = useRoute<RouteProp<GuideStackParamList, "GuideDetailScreen">>();
-
   const { guide } = route.params;
 
+  const [currentTab, setCurrentTab] = useState<string>("정보");
+
+  const scrollView = useRef<ScrollView>(null);
+  const layoutWidth = Dimensions.get("window").width;
+
+  const handleTabPress = (tabName: string, index: number) => {
+    setCurrentTab(tabName);
+    scrollView.current?.scrollTo({ x: index * layoutWidth, animated: true });
+  };
+
+  const tabs = ["정보", "플랜", "서비스", "일정", "리뷰"];
+
+  const handlePageChange = (event: any) => {
+    const { contentOffset, layoutMeasurement } = event.nativeEvent;
+    const pageWidth = layoutMeasurement.width;
+    const currentPage = Math.floor(contentOffset.x / pageWidth);
+
+    tabs.forEach((tab, index) => {
+      if (currentPage === index) {
+        setCurrentTab(tab);
+      }
+    });
+  };
   return (
     <Screen>
-      <View>
-        <ImageBackground
-          source={{ uri: guide.photo }}
-          style={{ width: "100%", height: 400, opacity: 0.6 }}
-        >
-          <View
-            style={{ position: "absolute", left: 30, bottom: 30, zIndex: 1 }}
-          >
-            <View style={{ flexDirection: "row", alignItems: "flex-end" }}>
-              <Text style={{ fontSize: 40 }}>{guide.name}</Text>
-              <Text style={{ fontSize: 20, marginLeft: 10 }}>
-                {calculateAgeRange(guide.birthDate)}
-              </Text>
+      <ScrollView>
+        <View>
+          <ImageBackground source={{ uri: guide.photo }} style={styles.image}>
+            <View style={styles.infoContainer}>
+              <View style={{ flexDirection: "row", alignItems: "flex-end" }}>
+                <Text style={styles.name}>{guide.name}</Text>
+                <Text style={styles.age}>
+                  {calculateAgeRange(guide.birthDate)}
+                </Text>
+              </View>
+              <Text>n시간 전 접속</Text>
             </View>
-            <Text>n시간 전 접속</Text>
-          </View>
-          <View
-            style={{
-              position: "absolute",
-              right: 10,
-              alignItems: "center",
-              top: "30%",
-            }}
-          >
-            <Text>{guide.temp}℃</Text>
-            <TempBar style={{ width: 10, height: 130 }} progress={guide.temp} />
-          </View>
-        </ImageBackground>
-      </View>
+            <View style={styles.tempBarContainer}>
+              <Text>{guide.temp}℃</Text>
+              <TempBar style={styles.tempBar} progress={guide.temp} />
+            </View>
+          </ImageBackground>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: 10,
+          }}
+        >
+          {tabs.map((tab, index) => (
+            <Pressable
+              key={index}
+              style={styles.tabStyle}
+              onPress={() => handleTabPress(tab, index)}
+            >
+              <Text
+                style={[
+                  styles.tabText,
+                  {
+                    color: currentTab === tab ? "black" : "gray",
+                  },
+                ]}
+              >
+                {tab}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+      </ScrollView>
     </Screen>
   );
 }
 
 export default GuideDetailScreen;
+
+const styles = StyleSheet.create({
+  image: { width: "100%", height: 400, opacity: 0.6 },
+  infoContainer: { position: "absolute", left: 30, bottom: 30, zIndex: 1 },
+  name: { fontSize: 40 },
+  age: { fontSize: 20, marginLeft: 10 },
+  tempBarContainer: {
+    position: "absolute",
+    right: 10,
+    alignItems: "center",
+    top: "30%",
+  },
+  tempBar: { width: 10, height: 130 },
+  tabStyle: {
+    width: "20%",
+    alignItems: "center",
+  },
+  tabText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
