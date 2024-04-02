d90af9850f89e28df571f24e9302ae60353c7b4d
0gene68 <yeonghun8309@gmail.com>
2024-04-02 12:52:35 +0900

    feat: 가이드 리뷰 탭 추가

---
+++ src/components/guide/GuideDetailReview.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function GuideDetailReview() {
+  return (
+    <View style={styles.container}>
+      <Text>GuideDetailReview</Text>
+    </View>
+  );
+}
+
+export default GuideDetailReview;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "20%",
+    height: 200,
+    borderWidth: 1,
+  },
+});
