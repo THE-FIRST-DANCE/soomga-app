ff659e002dfe1a77b83732504f8ff200bf0497ef
0gene68 <yeonghun8309@gmail.com>
2024-04-02 22:15:09 +0900

    feat: 툴 모달 각 항목 추가

--- src/components/guide/ToolButton.tsx
+++ src/components/guide/ToolButton.tsx
@@ -1,24 +1,65 @@
-import { View, Text, StyleSheet } from "react-native";
-import { Feather } from "@expo/vector-icons";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Feather, SimpleLineIcons, Ionicons } from "@expo/vector-icons";
 import Colors from "@/modules/Color";
 
-function ToolButton() {
+export function ToolModal() {
   return (
-    <View style={styles.container}>
-      <Feather name="menu" size={40} color="black" />
+    <View style={styles.modalContainer}>
+      <View style={styles.modalItems}>
+        <SimpleLineIcons name="user-follow" size={24} color={Colors.BLACK} />
+        <Text style={styles.modalText}>팔로우</Text>
+      </View>
+      <View style={styles.modalItems}>
+        <Ionicons name="chatbubbles-outline" size={24} color={Colors.BLACK} />
+        <Text style={styles.modalText}>채팅하기</Text>
+      </View>
+      <View style={styles.modalItems}>
+        <Ionicons name="warning-outline" size={24} color={Colors.DANGER} />
+        <Text style={[styles.modalText, { color: Colors.DANGER }]}>
+          신고하기
+        </Text>
+      </View>
     </View>
+  );
+}
+
+function ToolButton({ onPress }: { onPress: () => void }) {
+  return (
+    <Pressable style={styles.buttonContainer} onPress={onPress}>
+      <Feather name="menu" size={35} color="black" />
+    </Pressable>
   );
 }
 
 export default ToolButton;
 
 const styles = StyleSheet.create({
-  container: {
+  modalContainer: {
+    position: "absolute",
+    bottom: 60,
+    right: 40,
+    width: 150,
+    height: 150,
+    borderWidth: 1,
+    borderColor: Colors.BASKETBALL_ORANGE,
+    borderRadius: 10,
+    backgroundColor: Colors.WHITE,
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 5,
+  },
+  modalItems: {
+    width: 110,
+    marginVertical: 8,
+    flexDirection: "row",
+  },
+  modalText: { fontSize: 20, marginLeft: 15 },
+  buttonContainer: {
     position: "absolute",
     right: 20,
     bottom: 20,
-    width: 70,
-    height: 70,
+    width: 60,
+    height: 60,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: Colors.WHITE,
