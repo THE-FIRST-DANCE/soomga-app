321f4b199d0b25056b6e097985e08ddd271f7b83
0gene68 <yeonghun8309@gmail.com>
2024-04-02 12:51:53 +0900

    feat: 가이드 플랜 탭 추가

---
+++ src/components/guide/GuideDetailPlan.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function GuideDetailPlan() {
+  return (
+    <View style={styles.container}>
+      <Text>GuideDetailPlan</Text>
+    </View>
+  );
+}
+
+export default GuideDetailPlan;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "20%",
+    height: 200,
+    borderWidth: 1,
+  },
+});
