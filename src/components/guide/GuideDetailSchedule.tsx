0bc66f92d7350e1d80814d8020251dae01cd427e
0gene68 <yeonghun8309@gmail.com>
2024-04-02 12:52:21 +0900

    feat: 가이드 일정 탭 추가

---
+++ src/components/guide/GuideDetailSchedule.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function GuideDetailSchedule() {
+  return (
+    <View style={styles.container}>
+      <Text>GuideDetailSchedule</Text>
+    </View>
+  );
+}
+
+export default GuideDetailSchedule;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "20%",
+    height: 200,
+    borderWidth: 1,
+  },
+});
