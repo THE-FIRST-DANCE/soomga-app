0113b10d8b43e7bda8ea735a7dfc64f0473ba136
0gene68 <yeonghun8309@gmail.com>
2024-04-03 20:23:24 +0900

    chore: 툴 버튼 UI 조정

---
+++ src/components/guide/ToolContainer.tsx
@@ -0,0 +1,123 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  Animated,
+  ViewStyle,
+} from "react-native";
+import { Feather, Ionicons, AntDesign } from "@expo/vector-icons";
+import Colors from "@/modules/Color";
+import React, { useEffect, useState } from "react";
+import { checkFollow } from "./GuideListPlan";
+import { GuideType } from "@/data/guides";
+
+function ToolContainer({
+  onPress,
+  guide,
+}: {
+  onPress: () => void;
+  guide: GuideType;
+}) {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  const [isFollowed, setIsFollowed] = useState<boolean>(false);
+
+  return (
+    <View style={styles.container}>
+      {isModalVisible && (
+        <View>
+          <View style={styles.modalItems}>
+            <Text style={styles.modalItemCaption}>
+              {isFollowed ? "팔로우함" : "팔로우"}
+            </Text>
+            <Pressable
+              style={styles.buttonContainer}
+              onPress={() => {
+                checkFollow({
+                  isFollowed,
+                  setIsFollowed,
+                  guideName: guide.name,
+                });
+              }}
+            >
+              {isFollowed ? (
+                <AntDesign name="heart" size={28} color={Colors.FEMALE_PINK} />
+              ) : (
+                <AntDesign name="hearto" size={28} color={Colors.BLACK} />
+              )}
+            </Pressable>
+          </View>
+          <View style={styles.modalItems}>
+            <Text style={styles.modalItemCaption}>채팅하기</Text>
+            <Pressable style={styles.buttonContainer} onPress={onPress}>
+              <Ionicons
+                name="chatbubbles-outline"
+                size={28}
+                color={Colors.BLACK}
+              />
+            </Pressable>
+          </View>
+          <View style={styles.modalItems}>
+            <Text style={styles.modalItemCaption}>신고하기</Text>
+            <Pressable style={styles.buttonContainer} onPress={onPress}>
+              <Ionicons
+                name="warning-outline"
+                size={28}
+                color={Colors.DANGER}
+              />
+            </Pressable>
+          </View>
+        </View>
+      )}
+      <Pressable
+        style={[styles.buttonContainer, { alignContent: "flex-end" }]}
+        onPress={() => setIsModalVisible(!isModalVisible)}
+      >
+        {isModalVisible ? (
+          <Ionicons name="close" size={35} color={Colors.BLACK} />
+        ) : (
+          <Feather name="menu" size={28} color={Colors.BLACK} />
+        )}
+      </Pressable>
+    </View>
+  );
+}
+
+export default ToolContainer;
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    right: 20,
+    bottom: 20,
+    alignItems: "flex-end",
+  },
+  modalItems: {
+    width: 150,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  modalItemCaption: {
+    backgroundColor: Colors.GRAY_MEDIUM,
+    textAlign: "center",
+    width: 80,
+    padding: 5,
+    borderRadius: 10,
+    fontSize: 20,
+  },
+  modalText: { fontSize: 20, marginRight: 15 },
+  buttonContainer: {
+    width: 60,
+    height: 60,
+    marginVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.WHITE,
+    borderWidth: 1,
+    borderColor: Colors.BASKETBALL_ORANGE,
+    borderRadius: 100,
+    elevation: 10,
+  },
+});
