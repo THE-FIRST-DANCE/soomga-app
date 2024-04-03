d999b48b003d7d3633a22db04e05a6bfc8baf651
hetame1 <poi8946@naver.com>
2024-04-03 14:23:45 +0900

    refactor: 설정 모달 분리

--- src/components/sos/FeedItem.tsx
+++ src/components/sos/FeedItem.tsx
@@ -21,6 +21,7 @@ import FeedComment from "./FeedComment";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { SosStackParamList } from "@/stacks/SosStack";
 import { BoardProcess } from "@/interface/share";
+import FeedSettingModal from "./FeedSettingModal";
 
 const FeedItem = ({ item }: { item: SosType }) => {
   const date = useFormatDate({ date: item.createdAt });
@@ -106,99 +107,25 @@ const FeedItem = ({ item }: { item: SosType }) => {
       </View>
 
       {/* 설정 버튼 */}
-      <TouchableOpacity
-        style={styles.settingButton}
-        onPress={() => setOpenSetting(true)}
-      >
-        <MaterialCommunityIcons name="dots-vertical" size={24} />
-
-        {/* 설정 모달 */}
-        <GlobalModal
-          type="bottom"
-          animation="slide"
-          visible={openSetting}
-          setVisible={setOpenSetting}
+      {item.author.id === 2 && (
+        <TouchableOpacity
+          style={styles.settingButton}
+          onPress={() => setOpenSetting(true)}
         >
-          <View style={styles.settingModal}>
-            <TouchableOpacity onPress={handleEdit} style={styles.iconText}>
-              <MaterialCommunityIcons
-                name="pencil-outline"
-                size={32}
-                color={Colors.GRAY_DARK}
-              />
-              <Text style={styles.settingText}>수정하기</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleDelete} style={styles.iconText}>
-              <MaterialCommunityIcons
-                name="delete-outline"
-                size={32}
-                color={Colors.DANGER}
-              />
-              <Text
-                style={[
-                  styles.settingText,
-                  {
-                    color: Colors.DANGER,
-                  },
-                ]}
-              >
-                게시물 삭제
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                setOpenProcess(true);
-              }}
-              style={styles.iconText}
-            >
-              <MaterialCommunityIcons
-                name="earth"
-                size={32}
-                color={Colors.GRAY_DARK}
-              />
-              <Text style={styles.settingText}>상태 변경하기</Text>
-            </TouchableOpacity>
+          <MaterialCommunityIcons name="dots-vertical" size={24} />
 
-            {/* 상태 변경 모달 */}
-            <GlobalModal
-              type="bottom"
-              animation="slide"
-              visible={openProcess}
-              setVisible={setOpenProcess}
-            >
-              <View style={styles.settingModal}>
-                <TouchableOpacity
-                  onPress={() => handleProcess(BoardProcess.ACTIVE)}
-                  style={styles.process}
-                >
-                  <Text style={styles.processText}>대기중</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => handleProcess(BoardProcess.PROCESSING)}
-                  style={styles.process}
-                >
-                  <Text style={styles.processText}>진행중</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => handleProcess(BoardProcess.COMPLETE)}
-                  style={styles.process}
-                >
-                  <Text
-                    style={[
-                      styles.processText,
-                      {
-                        color: Colors.GREEN,
-                      },
-                    ]}
-                  >
-                    완료
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </GlobalModal>
-          </View>
-        </GlobalModal>
-      </TouchableOpacity>
+          <FeedSettingModal
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+            handleProcess={handleProcess}
+            openSetting={openSetting}
+            setOpenSetting={setOpenSetting}
+            openProcess={openProcess}
+            setOpenProcess={setOpenProcess}
+            BoardProcess={BoardProcess}
+          />
+        </TouchableOpacity>
+      )}
 
       {/* 피드 내용 */}
       <View style={styles.feedContent}>
@@ -397,29 +324,5 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 15,
     top: 15,
-  },
-  settingModal: {
-    position: "relative",
-    minHeight: 100,
-    padding: 10,
-    gap: 20,
-  },
-  iconText: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 15,
-  },
-  settingText: {
-    fontSize: 18,
-  },
-  process: {
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: Colors.GRAY_MEDIUM,
-  },
-  processText: {
-    fontSize: 20,
   },
 });
