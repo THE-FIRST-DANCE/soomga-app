dfcd87decbaf90fdd3f8b9c0e5d86986bbce86ca
hetame1 <poi8946@naver.com>
2024-04-08 19:36:28 +0900

    feat: 로그인, 회원가입 관련 스택

---
+++ src/stacks/SignStack.tsx
@@ -0,0 +1,23 @@
+import SignInScreen from "@/Screens/SignScreen/SignInScreen";
+import SignUpScreen from "@/Screens/SignScreen/SignUpScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+
+export type SignStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
+};
+
+const Stack = createStackNavigator<SignStackParamList>();
+
+export default function SignStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="SignIn" component={SignInScreen} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+    </Stack.Navigator>
+  );
+}
