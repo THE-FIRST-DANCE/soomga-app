c2148850c26933c8924115d2f7cfef03974afe86
0gene68 <yeonghun8309@gmail.com>
2024-04-09 10:34:29 +0900

    feat: 채팅 리스트 페이지

--- src/Screens/ChatScreen/ChatListScreen.tsx
+++ src/Screens/ChatScreen/ChatListScreen.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 import { useRecoilValue } from "recoil";
 
-function ChatRoom() {
+function ChatListScreen() {
   return (
     <Screen title="채팅방 목록">
       <View style={styles.container}></View>
@@ -12,7 +12,7 @@ function ChatRoom() {
   );
 }
 
-export default ChatRoom;
+export default ChatListScreen;
 
 const styles = StyleSheet.create({
   container: {
