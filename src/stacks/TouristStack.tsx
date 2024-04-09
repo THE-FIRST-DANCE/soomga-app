8efda056573542835a17c7c5258514a9c1795eaa
hetame1 <poi8946@naver.com>
2024-04-09 18:58:44 +0900

    feat: 여행지 추천 페이지 추가

--- src/stacks/TouristStack.tsx
+++ src/stacks/TouristStack.tsx
@@ -1,9 +1,22 @@
+import TouristCommentEditScreen from "@/Screens/TouristScreen/TouristCommentEditScreen";
+import TouristDetailCommentsScreen from "@/Screens/TouristScreen/TouristDetailCommentsScreen";
+import TouristDetailScreen from "@/Screens/TouristScreen/TouristDetailScreen";
 import TouristScreen from "@/Screens/TouristScreen/TouristScreen";
+import { SosCommentType } from "@/interface/Sos";
 import { createStackNavigator } from "@react-navigation/stack";
 
 export type TouristStackParamList = {
   TouristScreen: undefined;
   TouristDetailScreen: {
+    touristId: number;
+  };
+  TouristDetailCommentsScreen: {
+    comments: SosCommentType[] | undefined;
+    touristId: number;
+  };
+  TouristCommentEditScreen: {
+    comment: string;
+    commentId: number;
     touristId: number;
   };
 };
@@ -18,6 +31,18 @@ export default function TouristStack() {
       }}
     >
       <Stack.Screen name="TouristScreen" component={TouristScreen} />
+      <Stack.Screen
+        name="TouristDetailScreen"
+        component={TouristDetailScreen}
+      />
+      <Stack.Screen
+        name="TouristDetailCommentsScreen"
+        component={TouristDetailCommentsScreen}
+      />
+      <Stack.Screen
+        name="TouristCommentEditScreen"
+        component={TouristCommentEditScreen}
+      />
     </Stack.Navigator>
   );
 }
