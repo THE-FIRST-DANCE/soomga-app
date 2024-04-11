dd5a41395a932026c93becc52647f4e620a23c0a
0gene68 <yeonghun8309@gmail.com>
2024-04-11 13:20:37 +0900

    feat: 채팅방 리스트 페이지 디자인

--- src/Screens/ChatScreen/ChatListScreen.tsx
+++ src/Screens/ChatScreen/ChatListScreen.tsx
@@ -1,13 +1,50 @@
 import Screen from "@/components/Screen";
-import { UserRecoil } from "@/state/store/UserRecoil";
+import { guides } from "@/data/guides";
+import { ChatStackParamList } from "@/stacks/ChatStack";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
-import { useRecoilValue } from "recoil";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 function ChatListScreen() {
+  const navigation = useNavigation<NavigationProp<ChatStackParamList>>();
+
   return (
     <Screen title="채팅방 목록">
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <FlatList
+          data={guides}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              activeOpacity={0.6}
+              style={styles.chatItem}
+              onPress={() =>
+                navigation.navigate("ChatRoomScreen", { guide: item })
+              }
+            >
+              <View style={{ flexDirection: "row" }}>
+                <Image
+                  source={{ uri: item.photo }}
+                  style={{ width: 60, height: 60, borderRadius: 20 }}
+                />
+                <View style={{ marginLeft: 10, flex: 1 }}>
+                  <Text style={styles.name}>{item.name}</Text>
+                  <Text style={styles.message}>메시지</Text>
+                </View>
+                <View>
+                  <Text>12:00</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        ></FlatList>
+      </View>
     </Screen>
   );
 }
@@ -17,6 +54,18 @@ export default ChatListScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
+  },
+  chatItem: {
+    height: 100,
+    padding: 10,
+    justifyContent: "center",
+    borderWidth: 1,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  message: {
+    marginTop: 10,
   },
 });
