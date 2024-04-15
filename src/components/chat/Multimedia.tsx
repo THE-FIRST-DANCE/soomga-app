b73404b3199eac22bce0594771554309000a0837
0gene68 <yeonghun8309@gmail.com>
2024-04-15 21:56:10 +0900

    feat: 멀티미디어(사진, 플랜, 예약) 디자인

--- src/components/chat/Multimedia.tsx
+++ src/components/chat/Multimedia.tsx
@@ -1,11 +1,62 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Feather, MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
+import Colors from "@/modules/Color";
+
+const tabs = [
+  {
+    id: 1,
+    name: "사진",
+    icon: <Feather name="image" size={33} color={Colors.WHITE} />,
+    backgroundColor: "#88d458",
+  },
+  {
+    id: 2,
+    name: "플랜",
+    icon: <Ionicons name="airplane" size={32} color={Colors.WHITE} />,
+    backgroundColor: "#6492dd",
+  },
+  {
+    id: 3,
+    name: "예약",
+    icon: (
+      <MaterialCommunityIcons
+        name="calendar-check"
+        size={33}
+        color={Colors.WHITE}
+      />
+    ),
+    backgroundColor: "#f56a71",
+  },
+];
 
 function Multimedia() {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      {tabs.map((tab, index) => (
+        <View
+          key={index}
+          style={{ width: 90, height: 90, alignItems: "center" }}
+        >
+          <View style={[styles.tab, { backgroundColor: tab.backgroundColor }]}>
+            {tab.icon}
+          </View>
+          <Text style={{ fontSize: 18 }}>{tab.name}</Text>
+        </View>
+      ))}
     </View>
   );
 }
 
 export default Multimedia;
+
+const styles = StyleSheet.create({
+  container: { padding: 20, flexDirection: "row" },
+  tab: {
+    width: 60,
+    height: 60,
+    marginBottom: 10,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
