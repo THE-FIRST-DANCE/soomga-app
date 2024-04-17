c1faa49fcb4e089652732069d3a5cc039b6ffa2c
0gene68 <yeonghun8309@gmail.com>
2024-04-17 12:49:24 +0900

    feat: 플랜 공유 모달/예약 신청 모달 렌더링하는 로직 추가

--- src/components/chat/Multimedia.tsx
+++ src/components/chat/Multimedia.tsx
@@ -1,35 +1,48 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Feather, MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 import Colors from "@/modules/Color";
-
-const tabs = [
-  {
-    id: 1,
-    name: "사진",
-    icon: <Feather name="image" size={33} color={Colors.WHITE} />,
-    backgroundColor: "#88d458",
-  },
-  {
-    id: 2,
-    name: "플랜",
-    icon: <Ionicons name="airplane" size={32} color={Colors.WHITE} />,
-    backgroundColor: "#6492dd",
-  },
-  {
-    id: 3,
-    name: "예약",
-    icon: (
-      <MaterialCommunityIcons
-        name="calendar-check"
-        size={33}
-        color={Colors.WHITE}
-      />
-    ),
-    backgroundColor: "#f56a71",
-  },
-];
+import { useState } from "react";
+import ChatPlanModal from "./ChatPlanModal";
+import ChatReservationModal from "./ChatReservationModal";
 
 function Multimedia() {
+  const [planVisible, setPlanVisible] = useState<boolean>(false);
+  const [reservationVisible, setReservationVisible] = useState<boolean>(false);
+
+  const tabs = [
+    {
+      id: 1,
+      name: "사진",
+      icon: <Feather name="image" size={33} color={Colors.WHITE} />,
+      backgroundColor: "#88d458",
+      /* FIX: 사진 기능 추가 후 구현 */
+      visible: null,
+      setVisible: null,
+    },
+    {
+      id: 2,
+      name: "플랜",
+      icon: <Ionicons name="airplane" size={32} color={Colors.WHITE} />,
+      backgroundColor: "#6492dd",
+      visible: planVisible,
+      setVisible: setPlanVisible,
+    },
+    {
+      id: 3,
+      name: "예약",
+      icon: (
+        <MaterialCommunityIcons
+          name="calendar-check"
+          size={33}
+          color={Colors.WHITE}
+        />
+      ),
+      backgroundColor: "#f56a71",
+      visible: reservationVisible,
+      setVisible: setReservationVisible,
+    },
+  ];
+
   return (
     <View style={styles.container}>
       {tabs.map((tab, index) => (
@@ -37,12 +50,26 @@ function Multimedia() {
           <TouchableOpacity
             activeOpacity={0.8}
             style={[styles.tab, { backgroundColor: tab.backgroundColor }]}
+            onPress={() => {
+              if (tab.visible !== null) {
+                tab.setVisible(!tab.visible);
+              }
+            }}
           >
             {tab.icon}
           </TouchableOpacity>
           <Text style={{ fontSize: 18 }}>{tab.name}</Text>
         </View>
       ))}
+      {planVisible && (
+        <ChatPlanModal visible={planVisible} setVisible={setPlanVisible} />
+      )}
+      {reservationVisible && (
+        <ChatReservationModal
+          visible={reservationVisible}
+          setVisible={setReservationVisible}
+        />
+      )}
     </View>
   );
 }
