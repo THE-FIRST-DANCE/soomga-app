74f9a1cb41415bab570cfcb0c0006d655180f434
0gene68 <yeonghun8309@gmail.com>
2024-04-30 12:30:20 +0900

    chore: 백엔드 요청 api 변경

--- src/api/PlanApi.ts
+++ src/api/PlanApi.ts
@@ -6,10 +6,14 @@ import {
 } from "@/interface/Plan";
 import axios from "axios";
 import { EXPO_PUBLIC_KAKAO_API } from "@env";
+import * as SecureStore from "expo-secure-store";
 
 export const api = axios.create({
-  baseURL: "http://192.168.0.20:3000/api/",
+  baseURL: "http://home.juhyeonni.co.kr:3000/api/",
   withCredentials: true,
+  headers: {
+    Authorization: "Bearer " + SecureStore.getItem("accessToken"),
+  },
 });
 
 export const getPlaceApi = async (category: string, region: string) => {
