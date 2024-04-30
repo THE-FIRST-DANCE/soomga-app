4cf085447d054c824886972da6e615f25cf4ad01
0gene68 <yeonghun8309@gmail.com>
2024-04-30 12:40:04 +0900

    feat: 채팅방 리스트 백엔드에서 가져오기

--- src/Screens/ChatScreen/ChatListScreen.tsx
+++ src/Screens/ChatScreen/ChatListScreen.tsx
@@ -1,8 +1,10 @@
+import { api } from "@/api/PlanApi";
 import Screen from "@/components/Screen";
 import { guides } from "@/data/guides";
 import { ChatStackParamList } from "@/stacks/ChatStack";
+import { User } from "@/state/store/UserRecoil";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -12,27 +14,50 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+interface ChatroomProps {
+  id: number;
+  name: string;
+  createdAt: Date;
+  deletedAt: Date;
+  members: User[];
+  messages: string[];
+}
+
 function ChatListScreen() {
   const navigation = useNavigation<NavigationProp<ChatStackParamList>>();
+
+  const [chatList, setChatList] = useState<ChatroomProps[]>([]);
+
+  const getChatList = async () => {
+    try {
+      const response = await api.get("chat");
+      setChatList(response.data);
+      console.log(chatList);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getChatList();
+  }, []);
 
   return (
     <Screen title="채팅방 목록">
       <View style={styles.container}>
         <FlatList
-          data={guides}
+          data={chatList}
           renderItem={({ item }) => (
             <TouchableOpacity
               activeOpacity={0.6}
               style={styles.chatItem}
-              onPress={() =>
-                navigation.navigate("ChatRoomScreen", { guide: item })
-              }
+              onPress={() => navigation.navigate("ChatRoomScreen")}
             >
               <View style={{ flexDirection: "row" }}>
-                <Image
+                {/* <Image
                   source={{ uri: item.photo }}
                   style={{ width: 60, height: 60, borderRadius: 20 }}
-                />
+                /> */}
                 <View style={{ marginLeft: 10, flex: 1 }}>
                   <Text style={styles.name}>{item.name}</Text>
                   <Text style={styles.message}>메시지</Text>
