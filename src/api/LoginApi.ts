4da6b67303eab43227344df161005cf2667d6b14
0gene68 <yeonghun8309@gmail.com>
2024-04-30 12:29:18 +0900

    feat: 이메일 로그인 로직 추가

--- src/api/LoginApi.ts
+++ src/api/LoginApi.ts
@@ -15,3 +15,25 @@ export const tokenLogin = async (token: string) => {
     console.error(error);
   }
 };
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
+export const login = async (loginForm: LoginForm) => {
+  try {
+    const response = await api.post("auth/signin", loginForm);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const response = await api.post("auth/signout");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
