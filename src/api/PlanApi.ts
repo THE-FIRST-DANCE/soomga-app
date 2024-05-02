77dd0f88e194771e67d8e23a389785d9a8a9bf78
0gene68 <yeonghun8309@gmail.com>
2024-05-02 21:53:53 +0900

    chore: 백엔드 주소 변경

--- src/api/PlanApi.ts
+++ src/api/PlanApi.ts
@@ -8,7 +8,7 @@ import axios from "axios";
 import { EXPO_PUBLIC_KAKAO_API } from "@env";
 
 export const api = axios.create({
-  baseURL: "http://192.168.0.20:3000/api/",
+  baseURL: "http://home.juhyeonni.co.kr:3000/api/",
   withCredentials: true,
 });
 
