2509761ecf7f97fc58983e816af5758374ebea9a
0gene68 <yeonghun8309@gmail.com>
2024-05-09 20:58:19 +0900

    feat: 가이드 리뷰 조회 함수 추가

--- src/api/GuideApi.ts
+++ src/api/GuideApi.ts
@@ -1,10 +1,27 @@
 import { api } from "./PlanApi";
 
+/* 가이드 리스트 조회 */
 export const getGuidesList = async () => {
   try {
     const response = await api.get("guides/search");
 
     console.log(response.data.items);
+    return response.data.items;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/* 가이드 리뷰 조회 */
+export const getGuideReviews = async (guideId: number) => {
+  try {
+    const response = await api.get(`guides/${guideId}/reviews`, {
+      params: {
+        guideId,
+      },
+    });
+
+    console.log(response.data.items, response.data.items.length);
     return response.data.items;
   } catch (error) {
     console.error(error);
