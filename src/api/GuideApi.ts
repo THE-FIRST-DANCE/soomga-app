57add0ce50407b1eb041b6d6d4597ffc18054798
0gene68 <yeonghun8309@gmail.com>
2024-05-14 09:12:31 +0900

    feat: 전체 지역 조회 함수 추가

--- src/api/GuideApi.ts
+++ src/api/GuideApi.ts
@@ -1,11 +1,21 @@
 import { api } from "./PlanApi";
 
+/* 전체 지역 조회 */
+export const getAreasList = async () => {
+  try {
+    const response = await api.get("areas");
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 /* 가이드 리스트 조회 */
 export const getGuidesList = async () => {
   try {
     const response = await api.get("guides/search");
 
-    console.log(response.data.items);
     return response.data.items;
   } catch (error) {
     console.error(error);
