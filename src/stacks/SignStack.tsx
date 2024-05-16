0fa424f7fd3ce6983cc9fd7b462dd4cf97421b41
0gene68 <yeonghun8309@gmail.com>
2024-05-16 09:42:16 +0900

    feat: 내 리뷰 페이지 이동

--- src/stacks/SignStack.tsx
+++ src/stacks/SignStack.tsx
@@ -1,3 +1,5 @@
+import { GuideReview } from "@/components/guide/GuideDetailReview";
+import MyReviewsScreen from "@/Screens/ProfileScreen/MyReviewsScreen";
 import SignInScreen from "@/Screens/SignScreen/SignInScreen";
 import SignUpScreen from "@/Screens/SignScreen/SignUpScreen";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -5,6 +7,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 export type SignStackParamList = {
   SignIn: undefined;
   SignUp: undefined;
+  MyReviewsScreen: undefined;
+  MyPlansScreen: undefined;
+  MyPlacesScreen: undefined;
+  ProfileModifyScreen: undefined;
+  GuideApplicationScreen: undefined;
+  LangSettingScreen: undefined;
 };
 
 const Stack = createStackNavigator<SignStackParamList>();
@@ -18,6 +26,7 @@ export default function SignStack() {
     >
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
+      <Stack.Screen name="MyReviewsScreen" component={MyReviewsScreen} />
     </Stack.Navigator>
   );
 }
