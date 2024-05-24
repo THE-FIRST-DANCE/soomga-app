a6b9c2ce015e680e3061db3ac5a948cbbc5bc70d
0gene68 <yeonghun8309@gmail.com>
2024-05-24 17:05:05 +0900

    feat: 언어 설정 페이지 이동 함수 추가

--- src/components/profile/ProfileSetting.tsx
+++ src/components/profile/ProfileSetting.tsx
@@ -1,8 +1,12 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import Colors from "@/modules/Color";
+import { SignStackParamList } from "@/stacks/SignStack";
+import { useNavigation, NavigationProp } from "@react-navigation/native";
 
 function ProfileSetting() {
+  const navigation = useNavigation<NavigationProp<SignStackParamList>>();
+
   return (
     <View>
       <TouchableOpacity style={styles.item} activeOpacity={0.6}>
@@ -13,7 +17,11 @@ function ProfileSetting() {
         <Text style={styles.title}>가이드 신청</Text>
         <AntDesign name="right" size={20} color="black" />
       </TouchableOpacity>
-      <TouchableOpacity style={styles.item} activeOpacity={0.6}>
+      <TouchableOpacity
+        style={styles.item}
+        activeOpacity={0.6}
+        onPress={() => navigation.navigate("LangSettingScreen")}
+      >
         <Text style={styles.title}>언어 설정</Text>
         <AntDesign name="right" size={20} color="black" />
       </TouchableOpacity>
