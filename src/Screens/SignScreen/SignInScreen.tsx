a899d096ef3af3e35f1f748e5ce82bdd52b1205a
0gene68 <yeonghun8309@gmail.com>
2024-06-07 14:34:19 +0900

    feat: 유저 데이터 존재하지 않는 경우 로그인 화면 띄우도록 표시

--- src/Screens/SignScreen/SignInScreen.tsx
+++ src/Screens/SignScreen/SignInScreen.tsx
@@ -76,8 +76,7 @@ const SignInScreen = () => {
     );
   };
 
-  // return user?.id ? (
-  return (
+  return user?.id ? (
     <View
       style={{
         flex: 1,
@@ -85,97 +84,96 @@ const SignInScreen = () => {
     >
       <Profile user={user} />
     </View>
+  ) : (
+    <View
+      style={{
+        padding: 20,
+      }}
+    >
+      {/* 제목, 부제목 */}
+      <View>
+        <Text style={styles.title}>어서오세요!</Text>
+        <Text style={styles.subtitle}>
+          여행의 시작, {"\n"}
+          <Text style={styles.soomgaText}>SOOMGA</Text>
+          <Text>와 함께라면</Text> {"\n"}
+          당신만의 특별한 여행이 펼쳐집니다.
+        </Text>
+      </View>
+      {/* 이메일, 비밀번호 입력창 */}
+      <View style={styles.inputContainer}>
+        <InputText title="이메일" placeholder="username@gmail.com" />
+        <View style={{ position: "relative" }}>
+          <InputText
+            title="비밀번호"
+            placeholder="비밀번호 입력"
+            style={{ marginTop: 20 }}
+            isPasswordVisible={isPasswordVisible}
+          />
+          <TouchableOpacity
+            activeOpacity={1}
+            onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+            style={styles.visibleButton}
+          >
+            {isPasswordVisible ? (
+              <Feather name="eye-off" size={24} color="black" />
+            ) : (
+              <Feather name="eye" size={24} color="black" />
+            )}
+          </TouchableOpacity>
+        </View>
+      </View>
+      {/* 회원가입, 비밀번호 재설정 버튼 */}
+      <View style={styles.signInButtonContainer}>
+        <TouchableOpacity style={{ marginTop: 10 }} activeOpacity={0.6}>
+          <Text style={styles.signInText}>회원가입</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ marginTop: 10 }} activeOpacity={0.6}>
+          <Text style={styles.signInText}>비밀번호를 잊어버렸나요?</Text>
+        </TouchableOpacity>
+      </View>
+      {/* 로그인 버튼 */}
+      <NextButton />
+      {/* 소셜 로그인 컨테이너 */}
+      <View>
+        <View
+          style={{ flexDirection: "row", alignItems: "center", marginTop: 20 }}
+        >
+          <Hr />
+          <Text style={{ marginHorizontal: 5, fontSize: 15 }}>소셜 로그인</Text>
+          <Hr />
+        </View>
+        <View style={styles.socialLoginContainer}>
+          {/* 구글 소셜 로그인 */}
+          <TouchableOpacity
+            activeOpacity={0.6}
+            style={styles.socialLoginButton}
+            onPress={googleLogin}
+          >
+            <GoogleIcon
+              style={{
+                width: 50,
+                height: 50,
+              }}
+            />
+          </TouchableOpacity>
+          {/* 라인 소셜 로그인 */}
+          <TouchableOpacity
+            activeOpacity={0.6}
+            style={styles.socialLoginButton}
+            onPress={lineLogin}
+          >
+            <LineIcon
+              style={{
+                width: 50,
+                height: 50,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
-  // ) : (
-  //   <View
-  //     style={{
-  //       padding: 20,
-  //     }}
-  //   >
-  //     {/* 제목, 부제목 */}
-  //     <View>
-  //       <Text style={styles.title}>어서오세요!</Text>
-  //       <Text style={styles.subtitle}>
-  //         여행의 시작, {"\n"}
-  //         <Text style={styles.soomgaText}>SOOMGA</Text>
-  //         <Text>와 함께라면</Text> {"\n"}
-  //         당신만의 특별한 여행이 펼쳐집니다.
-  //       </Text>
-  //     </View>
-  //     {/* 이메일, 비밀번호 입력창 */}
-  //     <View style={styles.inputContainer}>
-  //       <InputText title="이메일" placeholder="username@gmail.com" />
-  //       <View style={{ position: "relative" }}>
-  //         <InputText
-  //           title="비밀번호"
-  //           placeholder="비밀번호 입력"
-  //           style={{ marginTop: 20 }}
-  //           isPasswordVisible={isPasswordVisible}
-  //         />
-  //         <TouchableOpacity
-  //           activeOpacity={1}
-  //           onPress={() => setIsPasswordVisible(!isPasswordVisible)}
-  //           style={styles.visibleButton}
-  //         >
-  //           {isPasswordVisible ? (
-  //             <Feather name="eye-off" size={24} color="black" />
-  //           ) : (
-  //             <Feather name="eye" size={24} color="black" />
-  //           )}
-  //         </TouchableOpacity>
-  //       </View>
-  //     </View>
-  //     {/* 회원가입, 비밀번호 재설정 버튼 */}
-  //     <View style={styles.signInButtonContainer}>
-  //       <TouchableOpacity style={{ marginTop: 10 }} activeOpacity={0.6}>
-  //         <Text style={styles.signInText}>회원가입</Text>
-  //       </TouchableOpacity>
-  //       <TouchableOpacity style={{ marginTop: 10 }} activeOpacity={0.6}>
-  //         <Text style={styles.signInText}>비밀번호를 잊어버렸나요?</Text>
-  //       </TouchableOpacity>
-  //     </View>
-  //     {/* 로그인 버튼 */}
-  //     <NextButton />
-  //     {/* 소셜 로그인 컨테이너 */}
-  //     <View>
-  //       <View
-  //         style={{ flexDirection: "row", alignItems: "center", marginTop: 20 }}
-  //       >
-  //         <Hr />
-  //         <Text style={{ marginHorizontal: 5, fontSize: 15 }}>소셜 로그인</Text>
-  //         <Hr />
-  //       </View>
-  //       <View style={styles.socialLoginContainer}>
-  //         {/* 구글 소셜 로그인 */}
-  //         <TouchableOpacity
-  //           activeOpacity={0.6}
-  //           style={styles.socialLoginButton}
-  //           onPress={googleLogin}
-  //         >
-  //           <GoogleIcon
-  //             style={{
-  //               width: 50,
-  //               height: 50,
-  //             }}
-  //           />
-  //         </TouchableOpacity>
-  //         {/* 라인 소셜 로그인 */}
-  //         <TouchableOpacity
-  //           activeOpacity={0.6}
-  //           style={styles.socialLoginButton}
-  //           onPress={lineLogin}
-  //         >
-  //           <LineIcon
-  //             style={{
-  //               width: 50,
-  //               height: 50,
-  //             }}
-  //           />
-  //         </TouchableOpacity>
-  //       </View>
-  //     </View>
-  //   </View>
-  // );
 };
 
 export default SignInScreen;
