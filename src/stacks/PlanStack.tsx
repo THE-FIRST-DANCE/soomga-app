f482733c63ce2c0fc7fb415b473eb3de44cfeb0f
hetame1 <poi8946@naver.com>
2024-06-08 22:15:59 +0900

    feat: 플랜 페이지 props 변경

--- src/stacks/PlanStack.tsx
+++ src/stacks/PlanStack.tsx
@@ -7,6 +7,7 @@ import PlanCreateScreen from "@/Screens/PlanScreen/PlanCreateScreen";
 import PlaceSelectScreen from "@/Screens/PlanScreen/PlaceSelectScreen";
 import PlanPlaceSelectScreen from "@/Screens/PlanScreen/PlanPlaceSelectScreen";
 import PlanDetailScreen from "@/Screens/PlanScreen/PlanDetailScreen";
+import PlanExecuteScreen from "@/Screens/PlanScreen/PlanExecuteScreen";
 
 import { PlanConfirmPeriodList, Plans } from "@/interface/Plan";
 import { PlanInfo } from "@/state/store/PlanRecoil";
@@ -27,6 +28,10 @@ export type PlanStackParamList = {
     transport: string;
   };
   PlanDetailScreen: {
+    planId: number;
+  };
+  PlanExecuteScreen: {
+    executePlanId: number;
     planId: number;
   };
 };
@@ -50,6 +55,7 @@ export default function PlanStack() {
       <Stack.Screen name="PlanConfirmScreen" component={PlanConfirmScreen} />
       <Stack.Screen name="PlanEditScreen" component={PlanEditScreen} />
       <Stack.Screen name="PlanDetailScreen" component={PlanDetailScreen} />
+      <Stack.Screen name="PlanExecuteScreen" component={PlanExecuteScreen} />
     </Stack.Navigator>
   );
 }
