aa8d34663dfe38c47dc79f54df3761135cc23a24
0gene68 <yeonghun8309@gmail.com>
2024-06-19 22:35:46 +0900

    feat: 채팅방 메시지 가져오는 함수

--- src/api/ChatApi.ts
+++ src/api/ChatApi.ts
@@ -1,4 +1,4 @@
-import { Room } from "@/interface/Chat";
+import { MessagePaginate, Room } from "@/interface/Chat";
 import { api } from "./PlanApi";
 
 /* 채팅방 목록 가져오기 */
@@ -11,3 +11,19 @@ export const getRooms = async () => {
     return undefined;
   }
 };
+
+/* 채팅방 메시지 가져오기 */
+export async function getMessages(
+  roomId: string,
+  cursor?: number,
+  limit?: number
+) {
+  const res = await api.get<MessagePaginate>(`/chat/${roomId}/messages`, {
+    params: {
+      cursor,
+      limit,
+    },
+  });
+
+  return res.data;
+}
